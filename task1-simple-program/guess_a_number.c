b477b6a1e5a51ad865c9d9d0265e2568c98d003a
mirexcool <mirexcool@gmail.com>
2022-12-25 12:00:09 -0800

    task1-simple-program: Add functionality to the main function

--- task1-simple-program/guess_a_number.c
+++ task1-simple-program/guess_a_number.c
@@ -5,8 +5,21 @@
  */
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(void)
 {
+	srand(time(NULL)); // Seed
+	unsigned short randomNumber = rand() % 10; // Generate random number form 0 to 9
+	unsigned short userNumber = 0;
+
+	printf("Enter the number beetwen 0 and 9:\n");
+	scanf("%hu", &userNumber); // Read number from console
+	if (userNumber != randomNumber) {
+		printf("You loose\n");
+		return 1;
+	}
+	printf("You win\n");
 	return 0;
 }
