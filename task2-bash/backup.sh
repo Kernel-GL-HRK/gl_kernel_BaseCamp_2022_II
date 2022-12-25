3caaa7cd3c8a991926dd45eb28acc92d37a9c5f3
kmakhno <kostiantynmakhno@gmail.com>
2022-12-25 14:54:47 +0200

    Homework 2: Add check of existence release folder
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task2-bash/backup.sh
+++ task2-bash/backup.sh
@@ -2,10 +2,16 @@
 
 SOURCE_DIR=../task1-simple-program
 DEST_DIR=/tmp/guessanumber
+RELEASE_DIR=./release
 
 FILES=`find $SOURCE_DIR -name *.c`
 
 if [[ ! -d "$DEST_DIR" ]]
 then
 	mkdir -p $DEST_DIR
+fi
+
+if [[ ! -d $RELEASE_DIR ]]
+then
+	mkdir -p $RELEASE_DIR
 fi
