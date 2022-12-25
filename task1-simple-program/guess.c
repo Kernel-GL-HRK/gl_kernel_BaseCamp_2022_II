308126e3c7a45c33358999effe50be3050c89eb9
kmakhno <kostiantynmakhno@gmail.com>
2022-12-25 12:35:05 +0200

    Homework 1: Add simple main functionality
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task1-simple-program/guess.c
+++ task1-simple-program/guess.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(void)
 {
+	int user_val, gen_val, cmp;
+
+	scanf("%d\n", &user_val);
+
+	gen_val = rand() % 10;
+	cmp = (user_val == gen_val);
+	printf("You %s\n", cmp ? "win" : "loose");
+	
+	if (!cmp)
+		return -1;
+
 	return 0;
 }
