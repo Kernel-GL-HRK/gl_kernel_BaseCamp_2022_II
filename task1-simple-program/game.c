207905f8baf714a0654e7ad1e8863052ae2ba2c7
yurakl <yuraklisko@gmail.com>
2022-12-26 14:35:50 +0200

    Task 1: add main functionality in main() in game.c

--- task1-simple-program/game.c
+++ task1-simple-program/game.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-	return 0;
+	//user_n - number is defined by user via console input
+	//machine_n - number is difined randomly
+	unsigned int user_n = 0, machine_n = 0;
+
+	printf("Enter a number (0-9):\n");
+	scanf("%u", &user_n);
+
+	srand(time(NULL));
+	machine_n = rand() % 10;
+
+	if (user_n == machine_n) {
+		printf("You win\n");
+		return 0;
+	}
+	printf("You loose\n");
+	return 1;
 }
