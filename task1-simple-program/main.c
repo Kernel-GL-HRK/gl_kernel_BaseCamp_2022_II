7eccd0c656982451cab604a2e428086eaff08378
Dmytro Tereshchenko <buxdmo@gmail.com>
2022-12-26 14:57:42 +0200

    Homework 1: Create simple main.c file with empty main function
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

---
+++ task1-simple-program/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+        int input;
+
+        printf("Guess a number -> ");
+
+        if (!scanf("%d", &input)) {
+                printf("Wrong input\n");
+                return -1;
+        }
+
+        srand(time(NULL));
+
+        const int random = rand() % 10;
+
+        if (input == random) {
+                printf("You win %d = %d\n", input, random);
+                return 0;
+        }
+        else {
+                printf("You loose %d != %d\n", input, random);
+                return 1;
+        }
+}
