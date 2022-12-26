bf24fca842a553f78447815a000c0cff759c8905
yurakl <yuraklisko@gmail.com>
2022-12-26 16:15:27 +0200

    Task 2: add code to archive the copied source files

--- task2-bash/script.sh
+++ task2-bash/script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-source_dir="/tmp/guesanumber"
+source_dir="/tmp/guesanumber/"
 
-#check if "/tmp/guesanumber" exist, if not - create
+#check if source_dir  exist, if not - create
 if [[ ! -d $source_dir ]]; then
-	mkdir $source_dir
+	mkdir -p $source_dir
 fi
 
-cp ../task1-simple-program/*.c  -t /tmp/guesanumber/
-# cp ../task1-simple-program/*.c ../task1-simple-program/*.h -t /tmp/guesanumber/
+cp ../task1-simple-program/*.c  -t $source_dir
+# cp ../task1-simple-program/*.c ../task1-simple-program/*.h -t $source_dir
+
+
+#cd to source_dir is done to prevent saving full path in archives
+cd $source_dir
+tar -cvf "../$(basename $source_dir).tar"  "../$(basename $source_dir)"
+tar -czvf "../$(basename $source_dir).tar.gz"  "../$(basename $source_dir)" 
+cd $OLDPWD
