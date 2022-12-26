d9cf699625878bfb36fbb79fb8994983022d3625
yurakl <yuraklisko@gmail.com>
2022-12-26 14:50:40 +0200

    Task 1: add a separate function to get a random number

--- task1-simple-program/game.c
+++ task1-simple-program/game.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+//function to get a random namber in range min:max
+int get_a_rand_num(int min, int max)
+{
+	srand(time(NULL));
+	return min + rand() % (max - min + 1);
+}
+
 int main(void)
 {
 	//user_n - number is defined by user via console input
@@ -12,8 +20,8 @@ int main(void)
 	scanf("%u", &user_n);
 
 	srand(time(NULL));
-	machine_n = rand() % 10;
-
+	machine_n = get_a_rand_num(0, 9);
+	//printf("%u %u\n", machine_n, user_n);
 	if (user_n == machine_n) {
 		printf("You win\n");
 		return 0;
