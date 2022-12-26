d38ad1ac80c08b768397f558d89cae5ad6ab851f
yurakl <yuraklisko@gmail.com>
2022-12-26 19:05:15 +0200

    Task 3: change game.c code to work with dynlink libs

--- task3-make/game.c
+++ task3-make/game.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-#include "func.h"
+
+//#define  dynlink 
+
+#ifdef dynlink
+	#include <dlfcn.h>
+#endif
+
+#ifndef dynlink
+	 #include "func.h"
+#endif
+
 int main(void)
 {
 	//user_n - number is defined by user via console input
@@ -8,8 +18,26 @@ int main(void)
 
 	printf("Enter a number (0-9):\n");
 	scanf("%u", &user_n);
+	#ifdef dynlink
+		puts("dyn");
+		void *handle = dlopen("libfunc.so", RTLD_LAZY);
+		if(!handle)
+		{
+			printf("Can't find libfunc.so\n");
+			return -1;
+		} 
+		int (*get_a_rand_num)(int, int);
 
-	machine_n = get_a_rand_num(0, 9);
+		get_a_rand_num = (int (*)())dlsym(handle,"get_a_rand_num");
+   		if(dlerror() != NULL)
+		{
+			printf("Can't find libfunc.so\n");
+		}
+
+   		(*get_a_rand_num)(0, 9);
+	#else
+		machine_n = get_a_rand_num(0, 9);
+	#endif
 	//printf("%u %u\n", machine_n, user_n);
 
 	if (user_n == machine_n) {
