a1ba6454174dfa41ec2ef21f997a53d2571a9080
Dmytro Tereshchenko <buxdmo@gmail.com>
2022-12-26 15:30:47 +0200

    Homework 1: Add range constants and input check
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -7,18 +7,21 @@ int get_random_number(int lower, int upper) {
 }
 
 int main() {
+	const int upper = 9;
+	const int lower = 0;
+
 	int input;
 
-	printf("Guess a number -> ");
+	printf("Guess a number (%d-%d) -> ", lower, upper);
 
-	if (!scanf("%d", &input)) {
+	if (!scanf("%d", &input) || input < lower || input > upper) {
 		printf("Wrong input\n");
 		return -1;
 	}
 
 	srand(time(NULL));
 
-	const int random = get_random_number(0, 9);
+	const int random = get_random_number(lower, upper);
 
 	if (input == random) {
 		printf("You win %d = %d\n", input, random);
