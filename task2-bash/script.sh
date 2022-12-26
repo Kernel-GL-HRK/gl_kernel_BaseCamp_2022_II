447e54356105eddce0452852e528d8a0d63b44c4
yurakl <yuraklisko@gmail.com>
2022-12-26 15:28:47 +0200

    Task 2: copy source files from task1-simple-program to /tmp/guesanumber

--- task2-bash/script.sh
+++ task2-bash/script.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+source_dir="/tmp/guesanumber"
+
+#check if "/tmp/guesanumber" exist, if not - create
+if [[ ! -d $source_dir ]]; then
+	mkdir $source_dir
+fi
+
+cp ../task1-simple-program/*.c  -t /tmp/guesanumber/
+# cp ../task1-simple-program/*.c ../task1-simple-program/*.h -t /tmp/guesanumber/
