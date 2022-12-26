0f816b8f9209d5aef9e4042e7218dad39d72b0de
kmakhno <kostiantynmakhno@gmail.com>
2022-12-26 11:58:11 +0200

    Task 2: Add check file existence and execution permissions
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task2-bash/run_guessanumber.sh
+++ task2-bash/run_guessanumber.sh
@@ -3,3 +3,24 @@
 EXEC_PATH=../task1-simple-program
 EXEC_FILENAME=a.out
 
+#Counter of number successful attempts
+success=0
+
+#User input variable
+ask=""
+
+repeats=1
+
+
+if [[ ! -f "$EXEC_PATH/$EXEC_FILENAME" ]]
+then
+	echo "$EXEC_FILENAME doesn't exist" >&2
+	exit -1
+fi
+
+if [[ ! -x "$EXEC_PATH/$EXEC_FILENAME" ]]
+then
+	echo "Doesn't have execution permissions" >&2
+	exit -1
+fi
+
