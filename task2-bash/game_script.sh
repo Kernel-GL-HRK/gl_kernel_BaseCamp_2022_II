3806b959142bd0e1dd01673d1d9bf34e4d295b82
yurakl <yuraklisko@gmail.com>
2022-12-26 16:46:25 +0200

    Task 2.2: add y/n menu and function to call game.out

--- task2-bash/game_script.sh
+++ task2-bash/game_script.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+game="../task1-simple-program/game.out"
+
+game_func()
+{
+	$game
+	if [[ $? -eq 0 ]]; then
+		echo "Good job"
+	else
+		echo "Wish a good luck next time"
+	fi
+}
+
+choise='y'
+while [[ $choise != 'n' ]]; do
+	read -p "Wanna play a game?:  " choise
+	if [[ $choise = 'y' || $choise = 'Y' ]]; then
+		game_func
+	elif [[ $choise = 'n' || $choise = 'N' ]]; then
+		echo "Bye bye"
+	fi
+done
