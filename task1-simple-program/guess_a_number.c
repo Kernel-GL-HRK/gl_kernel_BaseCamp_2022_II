5dc19903e0f132334df18b08a0ace6381e74fa2e
mirexcool <mirexcool@gmail.com>
2022-12-27 04:00:37 -0800

    TASK1: Create guess_a_number.c and .gitignore files
    Create guess_a_number.c file with empty main() function. Also create .gitignore file to ignore usless files during commit.

---
+++ task1-simple-program/guess_a_number.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Simple Guess a number program.
+ *
+ * Written by Yevhen Yefimov (mirexcool@gmail.com)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
