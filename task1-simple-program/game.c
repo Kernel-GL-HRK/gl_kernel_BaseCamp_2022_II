9e8e016b5d4c978ea3baaa2a213f0a52deaf3aec
yurakl <yuraklisko@gmail.com>
2022-12-27 18:56:30 +0200

    TASK01: Add main functionality to game.c

--- task1-simple-program/game.c
+++ task1-simple-program/game.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(void)
 {
+	int user_n, comp_n;
+	printf("Enter a number (0-9):\t\n");
+	scanf("%d", &user_n);
+	srand(time(NULL));
+	comp_n = rand() % 10;
+	printf("%d\n", comp_n);
+	if(user_n != comp_n)
+	{
+		printf("You loose\n");
+		return 1;
+	}
+	printf("You win\n");
 	return 0;
 }
