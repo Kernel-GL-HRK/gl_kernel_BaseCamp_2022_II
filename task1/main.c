f7b53c2e8f83ad207203c311a1526cbd9ef06367
Roman Kulchytskyi <kulch.roman@gmail.com>
2022-12-27 09:20:42 +0200

    Task1: Add license to the header of the file
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>

--- task1/main.c
+++ task1/main.c
@@ -1,3 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * (C) Copyright 2022
+ * Author: Roman Kulchytskyi <kulch.roman@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
