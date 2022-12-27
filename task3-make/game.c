95a3d1ef0b10ad87a697ee53869ab6102c729d2d
yurakl <yuraklisko@gmail.com>
2022-12-27 21:45:07 +0200

    TASK03: Edit game.c after checkpatch

--- task3-make/game.c
+++ task3-make/game.c
@@ -17,18 +17,20 @@ int main(void)
 	printf("Enter a number (0-9):\t\n");
 	scanf("%d", &user_n);
 	#ifdef DYNLINK
-		void* lib_handle;       /* handle of the opened library */
+		void *lib_handle;       //handle of the opened library
+
 		lib_handle = dlopen("libfunc.so", RTLD_LAZY);
 		if (!lib_handle) {
-    			printf("Error during dlopen(): %s\n", dlerror());
-    			return -1;
+			printf("Error during dlopen(): %s\n", dlerror());
+			return -1;
 		}
 		int (*getrand)(int, int);
-		getrand = (int (*)(int, int))dlsym(lib_handle,"getrand");
-		if(dlerror() != NULL) {
-                        printf("Error during dlsym(): %s\n", dlerror());
-                        return -1;
-                }
+
+		getrand = (int (*)(int, int))dlsym(lib_handle, "getrand");
+		if (dlerror() != NULL) {
+			printf("Error during dlsym(): %s\n", dlerror());
+			return -1;
+		}
 		comp_n = getrand(0, 9);
 	#endif
 	#ifndef DYNLINK
