d34ea2d8a0bc116802429aed7eeffa08bd422070
yurakl <yuraklisko@gmail.com>
2022-12-27 19:53:30 +0200

    TASK02: Add dialogue(menu) to scr_2.sh

--- task2-bash/scr_2.sh
+++ task2-bash/scr_2.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/bash
+while true; do
+	read -p "Wanna play a game?:  " choice
+	if [[ ${choice} = 'y' || ${choice} = 'Y' ]]; then
+		echo "+"
+	elif [[ ${choice} = 'n' || ${choice} = 'N' ]]; then
+		exit 0;
+	elif [[ ${choice} =~ ^[0-9]+$ ]]; then
+		while [[ ${choice} -gt 0 ]]; do
+			echo ${choice}
+			choice=$((choice-1))
+		done
+	fi
+done
