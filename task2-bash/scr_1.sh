5f0a70838b3385391c4d41be4fa2ffe9b9cc6b5a
yurakl <yuraklisko@gmail.com>
2022-12-27 19:32:48 +0200

    TASK02: Add copying of the source code to /tmp/guesanumber

--- task2-bash/scr_1.sh
+++ task2-bash/scr_1.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/bash
+TMP_DIR="/tmp/guesanumber"
+RLS_DIR="../release"
+SRC_DIR="../task1-simple-program"
 
+
+#Check if TMP_DIR exist. If no - create it
+if [[ ! -d ${TMP_DIR} ]]; then
+	mkdir -p ${TMP_DIR}
+fi
+
+#Copy all .c an .h files  from SRC_DIR to TMP_DIR
+cp "${SRC_DIR}/"*.c "${SRC_DIR}/"*.h -t ${TMP_DIR}
