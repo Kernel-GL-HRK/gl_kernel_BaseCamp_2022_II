2bd4aeada2f3dc305e3ea6a77ad6062cc6c4ce12
Yurii.Bezkorovainyi <13736343+bezkor@users.noreply.github.com>
2022-12-27 15:33:16 +0200

    TASK01: Add Main functionality
    Modify code for realise of main functionflity and check codestyle

--- task1-simple-program/guess_game.c
+++ task1-simple-program/guess_game.c
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0+
-
+#include <stdio.h>
+#include <stdlib.h>
 int main(void)
 {
-return 0;
+int user_choice;
+
+printf("User guess [0-9]:");
+user_choice = getchar() - '0';
+if (user_choice < 0 || user_choice > 9) {
+	puts("Bad input");
+	return -1;
+}
+
+if (user_choice == rand() % 10) {
+	puts("You win");
+	return 0;
+}
+puts("You loose");
+return 1;
 }
