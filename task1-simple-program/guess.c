895362587d18b3b631f247ead408735ca185ffc8
kmakhno <kostiantynmakhno@gmail.com>
2022-12-27 15:24:54 +0200

    Task1.1: Implement guessanumber main functionality
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task1-simple-program/guess.c
+++ task1-simple-program/guess.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-	return 0;
+	int user_val, gen_val;
+
+	srand(time(NULL));
+	printf("Enter a number [0-9]: ");
+	scanf("%d\n", &user_val);
+	gen_val = rand() % 10;
+
+	if (user_val == gen_val) {
+		printf("You win\n");
+		return 0;
+	}
+
+	printf("You loose\n");
+
+	return -1;
 }
