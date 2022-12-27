56f856990a92b280f66046299cfcd04e365ad810
Vlad Degtyarov <deesyync@gmail.com>
2022-12-27 23:13:04 +0200

    Task01: Implement minimal functionality
    Implemented minimal functionality according to the task,
    including user input and generation on random number.
    Everything realized within the body of main() function
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>

--- task1-simple-program/guess.c
+++ task1-simple-program/guess.c
@@ -1,6 +1,26 @@
 // SPDX-License-Identifier: MIT
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main(void)
 {
-	return 0;
+	int num, rdm;
+
+	printf("Enter number from 0 to 9: ");
+	scanf("%d", &num);
+
+	srand(time(NULL));
+	rdm = rand() % 10;
+
+	printf("The number is %d\nYou entered %d\n", rdm, num);
+
+	if (num != rdm) {
+		puts("You lost!");
+		exit(1);
+	}
+
+	puts("You win!");
+	exit(0);
 }
