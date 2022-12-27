c442bb2be3ae56c74b4369eda12d95cbee203f97
yurakl <yuraklisko@gmail.com>
2022-12-27 19:41:35 +0200

    TASK02: Add code to tar.gz /tmp/guesanumber

--- task2-bash/scr_1.sh
+++ task2-bash/scr_1.sh
@@ -11,3 +11,13 @@ fi
 
 #Copy all .c an .h files  from SRC_DIR to TMP_DIR
 cp "${SRC_DIR}/"*.c "${SRC_DIR}/"*.h -t ${TMP_DIR}
+
+#Copy all .c an .h files  from SRC_DIR to TMP_DIR
+cp "${SRC_DIR}/"*.c "${SRC_DIR}/"*.h -t ${TMP_DIR}
+
+#Arcive copied files in TMP_DIR by tar
+#cd to TMP_DIR is used to avoid adding the full path in .tar 
+cd ${TMP_DIR}
+tar -cvf "../$(basename ${TMP_DIR}).tar" "../$(basename ${TMP_DIR})" 
+tar -czvf "../$(basename ${TMP_DIR}).tar.gz" "../$(basename ${TMP_DIR})"
+cd $OLDPWD
