826d972dc1fc5cd8e1381485e32b1ff34a758e97
Yurii.Bezkorovainyi <13736343+bezkor@users.noreply.github.com>
2022-12-27 16:08:46 +0200

    TASK01: Move random part in function
    Implement function who return random numeric 0-9

--- task1-simple-program/guess_game.c
+++ task1-simple-program/guess_game.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0+
 #include <stdio.h>
 #include <stdlib.h>
+int get_random_numeric(int);
+
 int main(void)
 {
 int user_choice;
@@ -12,10 +14,16 @@ if (user_choice < 0 || user_choice > 9) {
 	return -1;
 }
 
-if (user_choice == rand() % 10) {
+if (user_choice == get_random_numeric(user_choice)) {
 	puts("You win");
 	return 0;
 }
 puts("You loose");
 return 1;
+}
+
+
+int get_random_numeric(int user_choice){
+srand(user_choice);
+return rand()%10;
 }
