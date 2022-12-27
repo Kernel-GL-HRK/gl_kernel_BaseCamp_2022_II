3ebd66ea0405fe9bc37945b71e4ef1a40e23da9e
mirexcool <mirexcool@gmail.com>
2022-12-27 13:39:18 -0800

    TASK2: Add gameplay to script
    In file script_2.sh add infinity loop wich call new game ones(or N times) or exit the game. Before exit prints how many times player won the game.

--- task2-bash/script_2.sh
+++ task2-bash/script_2.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+GAME="../task1-simple-program/game"
+WINS=0
+
+new_game(){
+	$GAME
+	if [ $? -eq 0 ]
+	then
+		echo "Good job"
+		WINS=$((WINS+1))
+	else
+		echo "Wish a good luck next time"
+	fi
+}
+
+
+while true; do
+	read INPUT
+	if [ $INPUT = "y" ]; then
+		new_game
+	elif [[ $INPUT =~ [0-9] ]]; then
+		while [ $INPUT -gt 0 ]; do
+			new_game
+			INPUT=$((INPUT-1))
+		done
+	elif [ $INPUT = "n" ]; then
+		echo "Youre score: " $WINS
+		break
+	fi
+done
