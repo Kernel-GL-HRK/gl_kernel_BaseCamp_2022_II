52e07bf1095165e513da1044f221507f20c28d2a
yurakl <yuraklisko@gmail.com>
2022-12-27 19:07:04 +0200

    TASK01: Edit code after checkpatch

--- task1-simple-program/game.c
+++ task1-simple-program/game.c
@@ -12,12 +12,12 @@ int getrand(int min, int max)
 int main(void)
 {
 	int user_n, comp_n;
+
 	printf("Enter a number (0-9):\t\n");
 	scanf("%d", &user_n);
 	comp_n = getrand(0, 10);
 	printf("%d\n", comp_n);
-	if(user_n != comp_n)
-	{
+	if (user_n != comp_n) {
 		printf("You loose\n");
 		return 1;
 	}
