f7e630a0596b1a241ef01af6bd91724669674f46
yurakl <yuraklisko@gmail.com>
2022-12-27 19:00:15 +0200

    TASK01: Add a separate function to get a random number

--- task1-simple-program/game.c
+++ task1-simple-program/game.c
@@ -2,13 +2,19 @@
 #include <time.h>
 #include <stdlib.h>
 
+// the function returns a random integer in range (min:max)
+int getrand(int min, int max)
+{
+	srand(time(NULL));
+	return min + rand() % (max - min + 1);
+}
+
 int main(void)
 {
 	int user_n, comp_n;
 	printf("Enter a number (0-9):\t\n");
 	scanf("%d", &user_n);
-	srand(time(NULL));
-	comp_n = rand() % 10;
+	comp_n = getrand(0, 10);
 	printf("%d\n", comp_n);
 	if(user_n != comp_n)
 	{
