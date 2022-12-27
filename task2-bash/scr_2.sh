a69d6def60ec5ed23b7be2890a013b61f5133f5a
yurakl <yuraklisko@gmail.com>
2022-12-27 20:02:53 +0200

    TASK02: Add function, which runs the game, in scr_2.sh

--- task2-bash/scr_2.sh
+++ task2-bash/scr_2.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/bash
+game="../task1-simple-program/game.out"
+
+call_game(){
+	$game
+	if  [[ $? -eq 0 ]]; then
+		echo "Good job"
+	else
+		echo "Wish a good luck next time"
+	fi
+}
+
 while true; do
 	read -p "Wanna play a game?:  " choice
 	if [[ ${choice} = 'y' || ${choice} = 'Y' ]]; then
-		echo "+"
+		call_game
 	elif [[ ${choice} = 'n' || ${choice} = 'N' ]]; then
 		exit 0;
 	elif [[ ${choice} =~ ^[0-9]+$ ]]; then
 		while [[ ${choice} -gt 0 ]]; do
-			echo ${choice}
+			call_game
 			choice=$((choice-1))
 		done
 	fi
