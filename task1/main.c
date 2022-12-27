4a6d2354045405699f0880b6f33098ec3681b345
Roman Kulchytskyi <kulch.roman@gmail.com>
2022-12-27 09:03:22 +0200

    Task1: Fix formatting
    Apply clang-format to fix formatting
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>

--- task1/main.c
+++ task1/main.c
@@ -8,48 +8,47 @@ static const int HIGH = 9;
 // generates random number in range [low, high]
 int getRandomNumber(int low, int high)
 {
-    int result = 0, low_num = 0, hi_num = 0;
+	int result = 0, low_num = 0, hi_num = 0;
 
-    if (low < high) {
-        low_num = low;
-        hi_num = high + 1; // include max_num in output
-    } else {
-        low_num = high + 1; // include max_num in output
-        hi_num = low;
-    }
+	if (low < high) {
+		low_num = low;
+		hi_num = high + 1; // include max_num in output
+	} else {
+		low_num = high + 1; // include max_num in output
+		hi_num = low;
+	}
 
-    srand(time(NULL));
-    result = (rand() % (hi_num - low_num)) + low_num;
-    return result;
+	srand(time(NULL));
+	result = (rand() % (hi_num - low_num)) + low_num;
+	return result;
 }
 
 // get user number in range [low, high]
 int getUserNumber(int low, int high)
 {
-    int userNumber = -1;
-    do {
-        scanf("%d", &userNumber);
-        if (userNumber < low || userNumber > high) {
-            printf("Number should be in range [%d, %d]\n", LOW,
-                   HIGH);
-            userNumber = -1;
-        }
-    } while (userNumber == -1);
+	int userNumber = -1;
+	do {
+		scanf("%d", &userNumber);
+		if (userNumber < low || userNumber > high) {
+			printf("Number should be in range [%d, %d]\n", LOW,
+			       HIGH);
+			userNumber = -1;
+		}
+	} while (userNumber == -1);
 
-    return userNumber;
+	return userNumber;
 }
 
 int main()
 {
-    printf("Enter number (%d - %d):\n", LOW, HIGH);
-    const int userNumber = getUserNumber(LOW, HIGH);
-    const int randomNumber = getRandomNumber(LOW, HIGH);
-    if (userNumber == randomNumber) {
-        printf("You guess the number %d!\n", userNumber);
-        return 0;
-    } else {
-        printf("You lose. Number was %d!\n", randomNumber);
-        return -1;
-    }
-
+	printf("Enter number (%d - %d):\n", LOW, HIGH);
+	const int userNumber = getUserNumber(LOW, HIGH);
+	const int randomNumber = getRandomNumber(LOW, HIGH);
+	if (userNumber == randomNumber) {
+		printf("You guess the number %d!\n", userNumber);
+		return 0;
+	} else {
+		printf("You lose. Number was %d!\n", randomNumber);
+		return -1;
+	}
 }
