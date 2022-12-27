e58ed8a4c8b4fe6584c7d69e2a2200cedb0a49e5
yurakl <yuraklisko@gmail.com>
2022-12-27 21:36:30 +0200

    TASK03: Edit game.c code for working with dynlink library

--- task3-make/game.c
+++ task3-make/game.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+
+#define DYNLINK
+
+#ifdef DYNLINK
+#include <dlfcn.h>
+#endif
+
+#ifndef DYNLINK
 #include "func.h"
+#endif
 
 int main(void)
 {
@@ -7,7 +16,24 @@ int main(void)
 
 	printf("Enter a number (0-9):\t\n");
 	scanf("%d", &user_n);
-	comp_n = getrand(0, 9);
+	#ifdef DYNLINK
+		void* lib_handle;       /* handle of the opened library */
+		lib_handle = dlopen("libfunc.so", RTLD_LAZY);
+		if (!lib_handle) {
+    			printf("Error during dlopen(): %s\n", dlerror());
+    			return -1;
+		}
+		int (*getrand)(int, int);
+		getrand = (int (*)(int, int))dlsym(lib_handle,"getrand");
+		if(dlerror() != NULL) {
+                        printf("Error during dlsym(): %s\n", dlerror());
+                        return -1;
+                }
+		comp_n = getrand(0, 9);
+	#endif
+	#ifndef DYNLINK
+		comp_n = getrand(0, 9);
+	#endif
 	printf("%d\n", comp_n);
 	if (user_n != comp_n) {
 		printf("You loose\n");
