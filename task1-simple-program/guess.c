7993b34f18c3cea69e2f96fde2c1bcbf7d00a063
kmakhno <kostiantynmakhno@gmail.com>
2022-12-27 15:29:28 +0200

    Task1.2: Move random generator to the separate function
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task1-simple-program/guess.c
+++ task1-simple-program/guess.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
+int get_rand(void)
+{
+	srand(time(NULL));
+	return rand() % 10;
+}
+
 int main(void)
 {
 	int user_val, gen_val;
 
-	srand(time(NULL));
 	printf("Enter a number [0-9]: ");
 	scanf("%d\n", &user_val);
-	gen_val = rand() % 10;
+	gen_val = get_rand();
 
 	if (user_val == gen_val) {
 		printf("You win\n");
