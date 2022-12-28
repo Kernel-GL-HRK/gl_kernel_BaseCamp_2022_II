d73a3d1c750f36415aa2b02eb6c9e3ae83b709e5
kmakhno <kostiantynmakhno@gmail.com>
2022-12-28 12:19:59 +0200

    Task2.2: Implement yes branch logic
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task2-bash/guessanumber.sh
+++ task2-bash/guessanumber.sh
@@ -10,13 +10,13 @@ repeats=1
 if [[ ! -f "$EXEC_PATH/$EXEC_FILENAME" ]]
 then
 	echo "$EXEC_FILENAME doesn't exist" >&2
-	exit -1
+#	exit -1
 fi
 
 if [[ ! -x "$EXEC_PATH/$EXEC_FILENAME" ]]
 then
 	echo "Doesn't have execution permissions" >&2
-	exit -1
+#	exit -1
 fi
 
 while [[ "$ask" != n ]]
@@ -25,4 +25,16 @@ do
 	echo ")type number to run program number times without a prompt"
 	echo ")type n to exit"
 	read ask
+
+	if [[ "$ask" == y ]]
+	then
+		if $EXEC_PATH/$EXEC_FILENAME
+		then
+			let success=$success+1
+			echo "Good job"
+			echo "Successful attempts: $success"
+		else
+			echo "Wish a good luck next time"
+		fi
+	fi
 done
