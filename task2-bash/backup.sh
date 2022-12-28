61400efbe921ed7ba721b14113effa73536687fc
kmakhno <kostiantynmakhno@gmail.com>
2022-12-28 06:40:43 +0200

    Task2.1: Add variable RELEASE for release path
    Add variable for release path make code more parameterized
    
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task2-bash/backup.sh
+++ task2-bash/backup.sh
@@ -2,12 +2,13 @@
 
 SRC_DIR=../task1-simple-program
 TMP_DIR=/tmp/guessanumber
+RELEASE=$(pwd)/release
 
 #find all source files in source folder
-FILES=`find $SOURCE_DIR -name '*.c' -or -name '*.h'`
+FILES=`find $SRC_DIR -name '*.c' -or -name '*.h'`
 
 #create folders
 mkdir -p $TMP_DIR 2>/dev/null
-mkdir -p $(pwd)/release 2>/dev/null
+mkdir -p $RELEASE 2>/dev/null
 
 cp $FILES $TMP_DIR
