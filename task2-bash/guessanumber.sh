542225b60f827ee669a8d9a7753c8ab0430b239b
kmakhno <kostiantynmakhno@gmail.com>
2022-12-28 11:46:58 +0200

    Task2.2: Add file existence condition
    Check if executable file exists to be able to run it later
    
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- task2-bash/guessanumber.sh
+++ task2-bash/guessanumber.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+EXEC_PATH=../task1-simple-program
+EXEC_FILENAME=guessanumber
+
+success=0
+ask=""
+repeats=1
+
+if [[ ! -f "$EXEC_PATH/$EXEC_FILENAME" ]]
+then
+	echo "$EXEC_FILENAME doesn't exist" >&2
+	exit -1
+fi
+
