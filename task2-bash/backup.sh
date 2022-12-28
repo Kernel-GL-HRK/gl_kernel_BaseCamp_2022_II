85c9046732b494f12a0a1eda758ec601abf412ee
Vlad Degtyarov <deesyync@gmail.com>
2022-12-29 01:07:27 +0200

    Task02: Add backup script
    Backup script implemented in backup.sh
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>

---
+++ task2-bash/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+src_folder="../task1-simple-program"
+tmp_folder="tmp/guesanumber"
+
+
+## Copying files to tmp folder
+mkdir -p $tmp_folder
+cp $src_folder/*.{c,h} $tmp_folder
+
+## Archiving backup to release folder
+mkdir -p $src_folder/release
+tar --directory=$(dirname $tmp_folder) -cvzf \
+	$src_folder/release/$(basename $tmp_folder).tar.gz \
+	$(basename $tmp_folder)
+	
