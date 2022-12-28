cea2e93c52e8151829d3be43ff92c7f29b57b9ce
mirexcool <mirexcool@gmail.com>
2022-12-28 06:11:18 -0800

    TASK3: Create Makefile and two source files for compiling
    Split source code from task1 on two files, also create header for this project and create simple Makefile with 'all' dependency.

---
+++ task3-make/guess_a_number.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+ /*
+  * Written by Yevhen Yefimov (mirexcool@gmail.com)
+  */
+
+#include "guess_a_number.h"
+
+int main(void)
+{
+	unsigned short randomNumber = 0;
+	unsigned short userNumber = 0;
+
+	randomNumber = random_generator_function();
+	printf("Enter the number beetwen 0 and 9:\n");
+	scanf("%hu", &userNumber); // Read number from console
+	if (userNumber != randomNumber) {
+		printf("You loose\n");
+		return 1;
+	}
+	printf("You win\n");
+	return 0;
+}
