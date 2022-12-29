b2c26f1f8c113f4de5ae5fa2c356fd0bfc9966bd
Vlad Degtyarov <deesyync@gmail.com>
2022-12-29 02:29:38 +0200

    Task02: Add cleanup of tmp folder before backup
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>

--- task2-bash/backup.sh
+++ task2-bash/backup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -ex
 
 src_folder="../task1-simple-program"
-tmp_folder="tmp/guesanumber"
+tmp_folder="/tmp/guesanumber"
 
+
+## Flushing tmp directory
+[ -d $tmp_folder ] && rm $tmp_folder/*
 
 ## Copying files to tmp folder
 mkdir -p $tmp_folder
@@ -13,4 +16,3 @@ mkdir -p $src_folder/release
 tar --directory=$(dirname $tmp_folder) -cvzf \
 	$src_folder/release/$(basename $tmp_folder).tar.gz \
 	$(basename $tmp_folder)
-	
