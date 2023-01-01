67c8c70d3579e4a75bb1d3a548fd3ce4c0e605ea
ra7e <artemnakorenko59@gmail.com>
2023-01-01 14:54:13 +0200

    TASK01:Fix coding style

--- task1-simple-program/src/main.c
+++ task1-simple-program/src/main.c
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022. All rights reserved.
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -7,27 +10,28 @@
 
 uint8_t generateRandomNumber(int lower, int upper)
 {
-    return rand() % (upper - lower + 1) + lower;
-} 
+	return rand() % (upper - lower + 1) + lower;
+}
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    uint8_t userNumber = 0;
-    printf("Please gimme a number in range [0-9]");
-    scanf("%hhd", &userNumber);
-    
-    if(userNumber < 0 || userNumber > 9){
-        printf("Value is not correct. Leaving\n");
-        return -1;
-    }
-    
-    if(userNumber == generateRandomNumber(LOWER, UPPER)){
-        printf("You win!\n");
-    }else {
-        printf("You lose\n");
-        return -1; 
-    }
-    
-    return 0;
+	uint8_t userNumber = 0;
+
+	printf("Please gimme a number in range [0-9]");
+	scanf("%hhd", &userNumber);
+
+	if (userNumber < 0 || userNumber > 9) {
+		printf("Value is not correct. Leaving\n");
+		return -1;
+	}
+
+	if (userNumber == generateRandomNumber(LOWER, UPPER)) {
+		printf("You win!\n");
+	} else {
+		printf("You lose\n");
+		return -1;
+	}
+
+	return 0;
 }
 
