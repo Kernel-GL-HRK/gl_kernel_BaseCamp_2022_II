51ae976cf88f220819514c904b8c873a3048ffa3
Yurii.Bezkorovainyi <13736343+bezkor@users.noreply.github.com>
2023-01-01 20:39:22 +0200

    TASK03: Add base Makefile targets
    Add some Makefile targets for future

--- task3-make/src/guess_game.c
+++ task3-make/src/guess_game.c
@@ -1,10 +1,8 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include "guess_lib.h"
 
-int main(){
-  printf("Results of input %i\n",get_user_input("Enter yn","yYnN"));
-  
-  return guess_game();
-
-return 0;
+int main()
+{
+	return guess_game();
+	return 0;
 }
