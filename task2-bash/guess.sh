af849721a5656ced9199f0825ca4ac1c6c6f463a
Vlad Degtyarov <deesyync@gmail.com>
2023-01-02 18:48:57 +0200

    Task02: Add execution loop
    Added execution loop to the game script
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>

--- task2-bash/guess.sh
+++ task2-bash/guess.sh
@@ -12,4 +12,53 @@ elif [ ! -x "$game_path/$game_name" ]; then
 	exit 2
 fi
 
-$game_path/$game_name
+total_tries=0
+win_tries=0
+
+
+run_game() {
+	$game_path/$game_name
+	
+	if [ $? -eq "0" ]; then
+		echo "Good job!"
+		((total_tries++))
+		((win_tries++))
+	elif [ $? -eq "1" ]; then
+		echo "Good luck next time!"
+		((total_tries++))
+	else
+		echo "Something goes wrong..."
+		exit 2;
+	fi
+}
+
+echo "Guess the number!"
+echo
+
+run_game
+
+while true; do
+
+	echo
+	echo "Wanna another try?"
+	read -p "Yes, No or Number of tries without this prompt (Y/n/0..99): " ans
+	
+	case $ans in
+	[0-9] | [0-9][0-9])
+		for n in $(seq $ans); do run_game; done
+		;;
+	[Nn])
+		break
+		;;
+	*)
+		run_game
+		;;
+	esac
+
+done
+
+echo
+echo "Lucky tries: $win_tries"
+echo "Total tries: $total_tries"
+echo "Bye!"
+
