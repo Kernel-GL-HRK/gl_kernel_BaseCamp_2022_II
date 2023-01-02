6abed25b85ffeccc27240622d3fd7027324c8818
Vlad Degtyarov <deesyync@gmail.com>
2023-01-02 18:42:11 +0200

    Task02: Add check for executable permission
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>

--- task2-bash/guess.sh
+++ task2-bash/guess.sh
@@ -7,6 +7,9 @@ game_name="guess"
 if [ ! -f "$game_path/$game_name" ]; then
 	echo "Can't find game executable!"
 	exit 1
+elif [ ! -x "$game_path/$game_name" ]; then
+	echo "Game file in not executable!"
+	exit 2
 fi
 
 $game_path/$game_name
