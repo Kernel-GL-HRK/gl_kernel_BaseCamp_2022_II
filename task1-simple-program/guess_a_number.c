e751810a0bb88a2cfab79c8fa0a0102cfca6dbbd
Viacheslav Us <slava.us@icloud.com>
2023-01-03 20:00:10 +0200

    TASK01: Implement random generate function
    Implementation of a function that returns a random number in a given range
    
    Signed-off-by: Viacheslav Us <slava.us@icloud.com>

--- task1-simple-program/guess_a_number.c
+++ task1-simple-program/guess_a_number.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_NUMBER 9
+#define MIN_NUMBER 0
+
+static inline uint8_t generate_number(void)
+{
+	return (uint8_t)(rand() % (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
+}
+
 int main(void)
 {
+	srand((unsigned int)time(NULL));
+
 	return 0;
 }
