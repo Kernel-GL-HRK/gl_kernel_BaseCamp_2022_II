d0b48fdc5e9420cdb9580676fa0ea7548ecbff9c
Sergiy Us <sergiy.us@gmail.com>
2023-01-03 21:01:25 +0200

    TASK01: Add a simple check
    Added simple verification of the number entered by the user.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -9,6 +9,7 @@
 
 #define EXIT_SUCCSESS	0
 #define EXIT_FAIL_1	1
+#define EXIT_FAIL_2	2
 
 int main(void)
 {
@@ -19,14 +20,20 @@ int main(void)
 	srand((unsigned int) time(NULL));
 	random_number = random() % 10;
 
+	printf("Please enter the number from 0 to 9: ");
 	scanf("%d", &user_number);
 
-	if (user_number == random_number) {
-		printf("You win\n");
-		exit_code = EXIT_SUCCSESS;
+	if ((user_number < 0) || (user_number > 9)) {
+		fprintf(stderr, "The number must be in the range from 0 to 9\n");
+		exit_code = EXIT_FAIL_2;
 	} else {
-		printf("You loose\n");
-		exit_code = EXIT_FAIL_1;
+		if (user_number == random_number) {
+			printf("You win\n");
+			exit_code = EXIT_SUCCSESS;
+		} else {
+			printf("You loose\n");
+			exit_code = EXIT_FAIL_1;
+		}
 	}
 
 	return exit_code;
