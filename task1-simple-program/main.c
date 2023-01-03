57a4e934a2b757e34eb1c8509120109c35930e8d
Sergiy Us <sergiy.us@gmail.com>
2023-01-03 15:24:15 +0200

    TASK01: Add main functionality
    In this commit added:
    1. The function of generating a random number in the range 0 to 9.
    2. Entering a number by the user.
    3. Comparing these numbers with output of the result.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -4,9 +4,30 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define EXIT_SUCCSESS	0
+#define EXIT_FAIL_1	1
 
 int main(void)
 {
+	unsigned int random_number;
+	unsigned int user_number;
+	int exit_code = 0;
 
-	return 0;
+	srand((unsigned int) time(NULL));
+	random_number = random() % 10;
+
+	scanf("%d", &user_number);
+
+	if (user_number == random_number) {
+		printf("You win\n");
+		exit_code = EXIT_SUCCSESS;
+	} else {
+		printf("You loose\n");
+		exit_code = EXIT_FAIL_1;
+	}
+
+	return exit_code;
 }
