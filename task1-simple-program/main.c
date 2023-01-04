57064f48f5b18cf7df93753058723cda818ff83f
Kushnir Vladyslav <vlad987321@gmail.com>
2023-01-04 20:05:12 +0200

    Task01: Edit code after checkpatch checking
    Signed-off-by: Kushnir Vladyslav <vlad987321@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -2,29 +2,29 @@
 #include <time.h>
 #include <stdlib.h>
 
-int get_rand(){
+int get_rand(void)
+{
 	srand(time(NULL));
 	return rand() % 10;
 }
 
-int main(void){
-	
-	int num;
+int main(void)
+{
+	int num = -1;
+
 	printf("Enter a number from 0 to 9: ");
 	scanf("%d", &num);
-	
-	if (num >= 0 && num<= 9){
-		if (num == get_rand()){
+	if (num >= 0 && num <= 9) {
+		if (num == get_rand()) {
 			printf("You win.\n");
 			return 0;
 		} else {
 			printf("You loose.\n");
 			return 1;
 		}
-		
-	} else{
+	} else {
 		printf("Wrong number, you loose.\n");
 		return 2;
-	}	
+	}
 }
 
