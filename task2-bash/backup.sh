368d2351c51c3bb457b35551a79ffa0f7c3aa3f6
Laura Shcherbak <laura.shcherbak@gmail.com>
2023-01-04 18:45:41 +0200

    Homework 2: Write main functionality for backup.sh
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>

--- task2-bash/backup.sh
+++ task2-bash/backup.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+SRC_CODE="../task1-simple-program/guess_a_number.c"
+PROGRAM="guesanumber"
+DEST_PATH="/tmp/$PROGRAM"
+ARCHIVE="$PROGRAM.tar.gz"
+
+#if [[ -d "$DEST_PATH" ]]; then
+#    rm -rf "$DEST_PATH"
+#fi
+mkdir "$DEST_PATH"
+
+#copping all source code of your project form part1 to /tmp/guesanumber.
+cp "$SRC_CODE" "$DEST_PATH"
+
+#if [[ -f "$ARCHIVE" ]]; then
+#    rm -f "$ARCHIVE"
+#fi
+
+#Compress this folder to gzip archive (please google tar command) with same name.
+tar -zcf "$ARCHIVE" --absolute-names "$DEST_PATH/"
+
+DEST_PATH="release"
+#if [[ -d "$DEST_PATH" ]]; then
+#    rm -rf "$DEST_PATH"
+#fi
+mkdir "$DEST_PATH"
+
+#Copy gzip archive to “release” subdirectory in project dir.
+cp "$PROGRAM.tar.gz" "$DEST_PATH"
