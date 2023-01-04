85dd1e50b5d8b2fc94c9927e79ca971bdc2a5ca6
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-01-04 19:11:38 +0200

    Homework 2: Add script state info
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task2-bash/script2.sh
+++ task2-bash/script2.sh
@@ -1,22 +1,26 @@
 #! bin/bash
 
 counter=1
+wins=0
+looses=0
 
 until [[ $counter -eq 0 ]]; do
-
+	echo "Tries: ${counter}\t Wins: ${wins}\t Looses: ${looses}"
         ((--counter))
 
         ../task1-simple-program/main.out
         ret=$?
 
         if [ $ret -eq 0 ]; then
-                echo "Good job"
+                echo -e "Good job"
+                ((++wins))
         elif [ $ret -eq 1 ]; then
-                echo "Wish a goog luck next time"
+                echo -e "Wish a goog luck next time"
+                ((++looses))
         fi
 
         if [ $counter -eq 0 ]; then
-        	echo -n "Want to continue (Y/N) | Number of tries ->"
+        		echo -n -e "Want to continue (Y/N) | Number of tries ->"
                 read reply
 
                 case $reply in
