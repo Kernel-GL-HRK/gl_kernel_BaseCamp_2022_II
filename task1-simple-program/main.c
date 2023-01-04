3c33f74f91b5f73ed92f86b1d13bd72903df73ba
Kushnir Vladyslav <vlad987321@gmail.com>
2023-01-04 19:45:32 +0200

    Task01: Implement the main function
    Signed-off-by: Kushnir Vladyslav <vlad987321@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -9,6 +9,22 @@ int get_rand(){
 
 int main(void){
 	
-	return 0;
+	int num;
+	printf("Enter a number from 0 to 9: ");
+	scanf("%d", &num);
+	
+	if (num >= 0 && num<= 9){
+		if (num == get_rand()){
+			printf("You win.\n");
+			return 0;
+		} else {
+			printf("You loose.\n");
+			return 1;
+		}
+		
+	} else{
+		printf("Wrong number, you loose.\n");
+		return 2;
+	}	
 }
 
