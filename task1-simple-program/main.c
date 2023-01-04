eff3a8d0d3e2de796d0c6ada1f150c5b05b5c407
Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>
2023-01-04 23:26:23 +0100

    TASK01: Add randNumber() function to file main.c and add scripts for colored text to main.h
    Connected library time.h for the function of randNumber()
    int randNumber(void) function for generating random numbers.
    Added scripts for displaying text in color in the console. To file main.h
    
    Signed-off-by: Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "main.h"
+
+int randNumber(void);
 
 int main(int argc, char **argv) {
 
+    int a, rand_num, var;
+
+    rand_num = randNumber();
+
+    printf("%s * %s%s        GAME \"Guess number!\"        %s\n",ON_IBLACK ,RESET ,ON_IBLUE ,RESET);
+    printf("%s * %s%s        guess the number *%sN%s%s*        %s\n",ON_IBLACK ,RESET ,ON_IBLUE ,BIWHITE, RESET, ON_IBLUE, RESET);
+
+
 return 0;
+}
+
+int randNumber(void) {
+    int n;
+
+  /* Intializes random number generator */
+    srand(time(NULL));
+    n = rand() % (9 + 1); // 0 - 9
+
+    return n;
 }
