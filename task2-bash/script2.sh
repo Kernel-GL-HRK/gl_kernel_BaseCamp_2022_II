69ff88726adafee564ef227058bfb4856b09f9ac
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-01-04 19:14:39 +0200

    Homework 2: Add colored text
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task2-bash/script2.sh
+++ task2-bash/script2.sh
@@ -1,5 +1,11 @@
 #! bin/bash
 
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 PATH=../task1-simple-program
 
 if [[ ! -f ${PATH}/main.out ]]; then
@@ -11,22 +17,23 @@ wins=0
 looses=0
 
 until [[ $counter -eq 0 ]]; do
-	echo "Tries: ${counter}\t Wins: ${wins}\t Looses: ${looses}"
+        echo -e ${BLUE}"Tries: ${counter}\t Wins: ${wins}\t Looses: ${looses}"${NC}
+
         ((--counter))
 
         ${PATH}/main.out
         ret=$?
 
         if [ $ret -eq 0 ]; then
-                echo -e "Good job"
+                echo -e ${GREEN}"Good job"${NC}
                 ((++wins))
         elif [ $ret -eq 1 ]; then
-                echo -e "Wish a goog luck next time"
+                echo -e ${RED}"Wish a goog luck next time"${NC}
                 ((++looses))
         fi
 
         if [ $counter -eq 0 ]; then
-        		echo -n -e "Want to continue (Y/N) | Number of tries ->"
+        		echo -n -e ${YELLOW}"Want to continue (Y/N) | Number of tries ->"${NC}
                 read reply
 
                 case $reply in
