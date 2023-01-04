4b06ec10e7cc6c95559a666e31ccd1a574595603
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-01-04 19:00:12 +0200

    Homework 2: Add some checks
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task2-bash/script1.sh
+++ task2-bash/script1.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
+if [[ ! -d /tmp/guessanumber ]]
+then
+        mkdir /tmp/guessanumber
+fi
+
 cp ../task1-simple-program/*.c /tmp/guessanumber
 
 tar -zcvf guessanumber.tar.gz /tmp/guessanumber
 
+if [[ ! -d ./release ]]
+then
+        mkdir release
+fi
+
 mv guessanumber.tar.gz release/
+
