a838ba7c4a33a8238c0879040a0135fbfa7c59b3
Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>
2023-01-05 01:00:34 +0100

    TASK01: Add user input validation module to main() function
    Checking input range from 0 to 9
    Checking for letters
    If the input is incorrect, the message (Please try again) appears
    
    Signed-off-by: Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -14,6 +14,16 @@ int main(int argc, char **argv) {
     printf("%s * %s%s        GAME \"Guess number!\"        %s\n",ON_IBLACK ,RESET ,ON_IBLUE ,RESET);
     printf("%s * %s%s        guess the number *%sN%s%s*        %s\n",ON_IBLACK ,RESET ,ON_IBLUE ,BIWHITE, RESET, ON_IBLUE, RESET);
 
+    do {
+        printf("%s * %s\n%s * %s     Enter an integer 0 - 9 : ",ON_IBLUE, RESET, ON_IBLUE, RESET);
+        var = scanf("%d", &a); // enter from the keyboard.
+        while ((getchar()) != '\n');
+        if ((var != 1) ||
+        ((a > 9) || (a < 0))) {
+            var = 0;
+            printf("%s * %s  %sPlease try again!%s\n", ON_IBLUE, RESET, RED_BOLD, RESET);
+        }
+
 
 return 0;
 }
