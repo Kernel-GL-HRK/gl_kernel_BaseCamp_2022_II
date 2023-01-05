2384e26810221d1cb653eb8fd04751f0b5b9b967
Viacheslav Us <slava.us@icloud.com>
2023-01-05 23:16:53 +0200

    TASK02.1: Implement copping all source code
    Add bask script that copies all source code from part1 to /tmp/guesanumber
    
    Signed-off-by: Viacheslav Us <slava.us@icloud.com>

--- task2-bash/script1.sh
+++ task2-bash/script1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash                                                                                                                    
+
+PART1_PATH=../task1-simple-program
+FOLDER=guesanumber
+COPY_PATH=/tmp/$FOLDER
+
+mkdir -p $COPY_PATH
+
+cp $PART1_PATH/*.[ch] $COPY_PATH
                                                                                    
