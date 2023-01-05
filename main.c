492df1719cfe162f3930b40046a4efbb15f27344
Maksym Khomenko <mr.homax@gmail.com>
2023-01-06 01:26:55 +0200

    Homework task1: Implement the game gues the number
    Signed-off-by: Maksym Khomenko <mr.homax@gmail.com>

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+int main(void)
 {
+int x, x_gues, ret_val;
 
-return 0;
+srand(time(NULL));
+printf("Enter your number in the range from 0 to 9\n");
+scanf("%u", &x_gues);
+x = rand() % 10;
+if (x == x_gues) {
+	printf("You Win!\n");
+	ret_val = 0;
+} else {
+	printf("You Lose\nThe lucky number was %d\n", x);
+	ret_val = -1;
+}
+return (ret_val);
 }
