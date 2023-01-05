b7d1fb881f087b70c8709eb5b6d8cf9da7766f75
Viacheslav Us <slava.us@icloud.com>
2023-01-06 01:36:06 +0200

    TASK02.2: Add infinity loop
    Add infinity loop in which game() is called
    
    Signed-off-by: Viacheslav Us <slava.us@icloud.com>

--- task2-bash/script2.sh
+++ task2-bash/script2.sh
@@ -3,6 +3,7 @@
 GAME=../task1-simple-program/guess_a_number
 
 win_count=0
+restart_count=1
 
 game() {
         $GAME
@@ -24,4 +25,17 @@ game() {
         esac
 }     
 
-game
+while :                                                                         
+do                                                                              
+                                                                                
+echo "WIN COUNT      = $win_count"                                              
+echo "RESTART COUNT  = $restart_count"                                          
+                                                                                
+if [ $restart_count -gt 0 ]                                                     
+then                                                                            
+        start_game                                                              
+        restart_count=$(($restart_count-1))                                     
+        continue                                                                
+fi                                                                              
+                                                                                
+done          
