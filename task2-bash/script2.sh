092123b8ef03cc239f1e32126cfd27104ca9bc40
Viacheslav Us <slava.us@icloud.com>
2023-01-06 00:59:05 +0200

    TASK02.2: Implement function game() that starts guess_a_number
    Function game() starts game and processes program exit code
    
    Signed-off-by: Viacheslav Us <slava.us@icloud.com>

--- task2-bash/script2.sh
+++ task2-bash/script2.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+GAME=../task1-simple-program/guess_a_number
+
+win_count=0
+
+game() {
+        $GAME
+        case $? in
+
+                0)
+                        win_count=$(($win_count+1))
+                        echo "Good job"
+                        ;;
+
+                1)
+                        echo "Wish a good luck next time"
+                        ;;
+
+                2)
+                        echo "You lost your attempt. Next time be careful"
+                        ;;
+
+        esac
+}     
+
+game
