e399f1394c1b44a00af11b53f7c49885bcbe9880
Viacheslav Us <slava.us@icloud.com>
2023-01-06 00:06:32 +0200

    TASK02.1: Add code to create gzip archive
    Add code to create gzip archive guesanumber.tar.gzip from /tmp/guesanumber
    
    Signed-off-by: Viacheslav Us <slava.us@icloud.com>

--- task2-bash/script1.sh
+++ task2-bash/script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                                                                                                    
 
-PART1_PATH=../task1-simple-program
+PART1_PATH=../task1-simple-program/
 FOLDER=guesanumber
 COPY_PATH=/tmp/$FOLDER
 
@@ -8,3 +8,7 @@ mkdir -p $COPY_PATH
 
 cp $PART1_PATH/*.[ch] $COPY_PATH
                                                                                    
+ARCHIVE=$FOLDER.tar.gzip
+
+cd $COPY_PATH/.. && tar -czf $ARCHIVE $FOLDER
+cd - > /dev/null
