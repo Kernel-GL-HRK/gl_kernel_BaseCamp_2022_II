8df808ed527d15e5ea265a6b6780727f29c301ce
saurier <kruzhanivska.olena212@gmail.com>
2023-01-07 01:00:14 +0200

    Task2.2: Add compress functionality
    Signed-off-by: saurier <kruzhanivska.olena212@gmail.com>

--- task2-bash/script.sh
+++ task2-bash/script.sh
@@ -2,4 +2,12 @@
 
 mkdir -p tmp/guessanumber
 
-cp ../task1-simple-program/guess.c ./tmp/guessanumber/
+cp ../task1-simple-program/*.c ./tmp/guessanumber/ 
+
+tar czf guessanumber.tar.gz ./tmp/guessanumber
+
+mkdir release
+
+cp guessanumber.tar.gz ./release/
+
+rm guessanumber.tar.gz
