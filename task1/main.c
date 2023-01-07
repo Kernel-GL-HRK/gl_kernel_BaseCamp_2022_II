f62c7ff3b9e08231cf0cc0e798fdb882bc700e91
Kulchytskyi Roman <kulch.roman@gmail.com>
2023-01-07 18:41:13 +0200

    Add code of the Guess a number game (#6)
    * Task1: Copy .clang-format file
    
    Copied .clang-format file from the linux repository
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Copy .gitignore file
    
    Copied .gitignore file from the linux repository
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Add game code
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Move all file to task1 subdirectory
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Remove implicit initiaalization for the static variable to 0
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Add blank line after variables declarations
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Fix formatting
    
    Apply clang-format to fix formatting
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Add blank line (code convention)
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Change function declaration main()->main(void)
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Remove useless else after return
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Add blank line after variable declaration (code convention)
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    * Task1: Add license to the header of the file
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>
    
    Signed-off-by: Roman Kulchytskyi <kulch.roman@gmail.com>

---
+++ task1/main.c
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * (C) Copyright 2022
+ * Author: Roman Kulchytskyi <kulch.roman@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+static const int LOW; // default to 0
+static const int HIGH = 9;
+
+// generates random number in range [low, high]
+int getRandomNumber(int low, int high)
+{
+	int result = 0, low_num = 0, hi_num = 0;
+
+	if (low < high) {
+		low_num = low;
+		hi_num = high + 1; // include max_num in output
+	} else {
+		low_num = high + 1; // include max_num in output
+		hi_num = low;
+	}
+
+	srand(time(NULL));
+	result = (rand() % (hi_num - low_num)) + low_num;
+	return result;
+}
+
+// get user number in range [low, high]
+int getUserNumber(int low, int high)
+{
+	int userNumber = -1;
+
+	do {
+		scanf("%d", &userNumber);
+		if (userNumber < low || userNumber > high) {
+			printf("Number should be in range [%d, %d]\n", LOW,
+			       HIGH);
+			userNumber = -1;
+		}
+	} while (userNumber == -1);
+
+	return userNumber;
+}
+
+int main(void)
+{
+	printf("Enter number (%d - %d):\n", LOW, HIGH);
+	const int userNumber = getUserNumber(LOW, HIGH);
+	const int randomNumber = getRandomNumber(LOW, HIGH);
+
+	if (userNumber == randomNumber) {
+		printf("You guess the number %d!\n", userNumber);
+		return 0;
+	}
+
+	printf("You lose. Number was %d!\n", randomNumber);
+	return -1;
+}
