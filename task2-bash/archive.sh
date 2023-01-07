48a152196246604e4ad06e2a72e3e5c76afdb349
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-01-07 21:44:56 +0000

    TASK02: Change archive.sh
    Variable names have been changed. The temporary directory path has been
    replaced by its name. To avoid errors, the mkdir command has been added.
    File extensions specified to copy source code only. The command to
    create an archive has been added.
    
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task2-bash/archive.sh
+++ task2-bash/archive.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# Path to temporary directory and project directory
-tmpath='/tmp/guesanumber'
-prpath='../task1-simple-program'
+# Temporary directory name and project directory path
+TEMP_NAME='guesanumber'
+PROJ_DIR='../task1-simple-program'
 
 # Copy all source code of my project form part 1
-cp -a "$prpath"/. "$tmpath"/
+mkdir -p /tmp/"$TEMP_NAME"/
+cp "$PROJ_DIR"/*.[ch] /tmp/"$TEMP_NAME"/
+
+# Compress temporary folder to gzip archive with same name
+tar -czvf "$TEMP_NAME".tar.gz /tmp/"$TEMP_NAME"
