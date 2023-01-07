dd009384e79d0a0ee0ffaa73ad0d8013705960e7
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-01-07 19:51:27 +0000

    TASK02: Modify archive.sh
    Wrote a feature that copies all of my project's source code from part 1
    to /tmp/guesanumber
    
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task2-bash/archive.sh
+++ task2-bash/archive.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Hello world!"
+# Path to temporary directory and project directory
+tmpath='/tmp/guesanumber'
+prpath='../task1-simple-program'
+
+# Copy all source code of my project form part 1
+cp -a "$prpath"/. "$tmpath"/
