3bedc84f2ba3aeeffd5e7356a47f65a2e7318f8c
deesync <83023299+deesync@users.noreply.github.com>
2023-01-07 19:33:56 +0200

    Task01: Practice git commiting according to naming convention (#32)
    * Homeworks 1. Create readme file
    
    Signed-off-by: Oleksii Vodka <oleksii.vodka@gmail.com>
    
    * Task01: Add file guess.c with empty main func
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>
    
    * Task01: Implement minimal functionality
    
    Implemented minimal functionality according to the task,
    including user input and generation on random number.
    Everything realized within the body of main() function
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>
    
    * Task01: Move random generation to own function
    
    Moved random generation out of main() function
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>
    
    * Task01: Add .gitignore
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>
    
    Signed-off-by: Oleksii Vodka <oleksii.vodka@gmail.com>
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>
    Co-authored-by: Oleksii Vodka <oleksii.vodka@gmail.com>

---
+++ task1-simple-program/guess.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int generate_number(void)
+{
+	srand(time(NULL));
+	return rand() % 10;
+}
+
+int main(void)
+{
+	int num, rdm;
+
+	printf("Enter number from 0 to 9: ");
+	scanf("%d", &num);
+
+	rdm = generate_number();
+	printf("The number is %d\nYou entered %d\n", rdm, num);
+
+	if (num != rdm) {
+		puts("You lost!");
+		exit(1);
+	}
+
+	puts("You win!");
+	exit(0);
+}
