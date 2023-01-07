88457e88d7ec63c71e2e2d8d3590fcc80c5acc7e
ra7e <artemnakorenko59@gmail.com>
2023-01-07 20:14:14 +0200

    TASK2: Add .gitignor; add coloring

--- task2-bash/task2_script.sh
+++ task2-bash/task2_script.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+RED='\033[0;31m'
+Blue='\033[0;34m'
+NC='\033[0m'
 
 src_dir=../task1-simple-program
 dest_dir=/tmp/guesanumber
 ar_name=guesanumber
-realese_dir=./release
+release_dir=./release
 
 create_or_check_dir(){
     if [[ ! -e $1 ]]; then
         mkdir $1
+        echo "${BLUE}Created $1 folder${NC}"
     elif [[ ! -d $1 ]]; then
-        echo "$1 already exists but is not a directory" 1>&2
+        echo "${RED}$1 already exists but is not a directory${NC}" 1>&2
     fi
 }
 
+echo -e "${BLUE}Checking destination folder - $dest_dir ${NC}"
 create_or_check_dir "$dest_dir"
 
-cp -R "$src_dir"/src "$dest_dir"
+cp -R $src_dir/src "$dest_dir"
 
 if [[ ! -e "$dest_dir"/src ]]; then
     echo "Destination folder doesnt contain Src folder"
 else
+    echo "${BLUE}Making archive${NC}"
     tar -zcvf "$ar_name".tar.gz "$dest_dir"/src
 fi
 
-create_or_check_dir "$realese_dir"
+echo "${BLUE}Checking release folder${NC}"
+create_or_check_dir "$release_dir"
 
-cp -R "$ar_name".tar.gz "$realese_dir"
+echo "${BLUE}Copying archive to release folder${NC}"
+cp -R "$ar_name".tar.gz "$release_dir"
