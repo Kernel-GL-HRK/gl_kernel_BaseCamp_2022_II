9f9fcc7cf67e80ae609f467d8bb9e85473921d7d
Maksym Khomenko <mr.homax@gmail.com>
2023-01-07 11:36:01 +0200

    Homework task2: Add in autostart.sh printing greetings depending on the guess number result on each iteration
    Signed-off-by: Maksym Khomenko <mr.homax@gmail.com>

--- autostart.sh
+++ autostart.sh
@@ -2,8 +2,15 @@
 
 while true; do
     ./main.out
-    
+    # Check the result and depending on it print greetings
+    if [ $? -eq 0 ]; then
+	echo "Good job!"
+    else 
+	echo "Wish a good luck next time."
+    fi
+ 
     read -p "Continue? (Y/N) or number of tries " reply
+
     if [ "$reply" = "N" ] || [ "$reply" = "n" ]; then
 	break
     fi
