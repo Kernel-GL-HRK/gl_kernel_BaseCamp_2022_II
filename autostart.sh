0c2e7e00147be0a2ccaae3e66cacfd6a19355658
Maksym Khomenko <mr.homax@gmail.com>
2023-01-07 13:27:05 +0200

    Homework task2: Add colours in autostart.sh prompts and replies
    Signed-off-by: Maksym Khomenko <mr.homax@gmail.com>

--- autostart.sh
+++ autostart.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 COUNT=0
+# Reset
+Color_Off='\033[0m' # Text Reset
+# Regular Colors
+Black='\033[0;30m'  # Black
+Red='\033[0;31m'    # Red
+Green='\033[0;32m'  # Green
+Yellow='\033[0;33m' # Yellow
+Blue='\033[0;34m'   # Blue
+Purple='\033[0;35m' # Purple
+Cyan='\033[0;36m'   # Cyan
+White='\033[0;97m'  # White
 
 while true; do
     ./main.out
     # Check the result and depending on it print greetings
     if [ $? -eq 0 ]; then
-	echo "Good job!"
+	echo -e "${Green}Good job!${White}"
     else 
-	echo "Wish a good luck next time."
+	echo -e "${Red}Wish a good luck next time.${White}"
     fi
     
     if [ $COUNT -eq 0 ]; then
-
-	read -p "Continue? (Y/N) or number of tries " reply
+	read -p "$(echo -e $Yellow'Continue? (Y/N) or number of tries  '$White)"  reply
+	# Check if reply is not a  number
 	if ! [[ $reply =~ ^[0-9]+$ ]]; then
 	    if [ "$reply" = "N" ] || [ "$reply" = "n" ]; then
 		break
@@ -24,6 +35,4 @@ while true; do
     else
 	let COUNT-=1
     fi
-    
-
 done
