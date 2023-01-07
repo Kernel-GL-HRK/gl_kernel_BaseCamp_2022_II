ecb2601eeaad669dc26590b41a6e6c30d5d51f76
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-01-07 22:10:22 +0000

    TASK02: Change archive.sh
    Wrote the name of the subdirectory to the variable. Feature released
    to copy archive to subdirectory.
    
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task2-bash/archive.sh
+++ task2-bash/archive.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# Temporary directory name and project directory path
-TEMP_NAME='guesanumber'
+# Temporary and sub- directory name and project directory path 
 PROJ_DIR='../task1-simple-program'
+TEMP_NAME='guesanumber'
+SDIR_NAME='release'
 
 # Copy all source code of my project form part 1
 mkdir -p /tmp/"$TEMP_NAME"/
@@ -10,3 +11,7 @@ cp "$PROJ_DIR"/*.[ch] /tmp/"$TEMP_NAME"/
 
 # Compress temporary folder to gzip archive with same name
 tar -czvf "$TEMP_NAME".tar.gz /tmp/"$TEMP_NAME"
+
+# Copy the gzip archive to the "release" subdirectory of the project directory
+mkdir "$SDIR_NAME"
+cp "$TEMP_NAME".tar.gz "$SDIR_NAME"
