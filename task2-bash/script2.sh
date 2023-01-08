e3ffa71cf11668408f5987b2269c45c94be66935
Serhii Shramko <shram.serhiy@gmail.com>
2023-01-08 18:16:02 +0200

    Task 2.2: Add exit from loop
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>

--- task2-bash/script2.sh
+++ task2-bash/script2.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 GAME=../a.out
+loop_count=1
 
 while true; do
     ./$GAME
     if [ $? -eq 0 ]; then
-        echo "Good job"
+        echo "Good job"$'\n'
     else
-        echo "Wish a good luck next time"
+        echo "Wish a good luck next time"$'\n'
+    fi
+    ((loop_count--))
+    if [ $loop_count -lt 1 ]; then
+        read -p "Continue y/n. Or number of loops: " answ
+        case $answ in
+            [Yy])
+                continue
+                ;;
+            [Nn])
+                break
+                ;;
+            *)
+                ((loop_count=answ))
+                ;;
+        esac
     fi
 done
