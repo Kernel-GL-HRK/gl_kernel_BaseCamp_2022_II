1e5840159f261f757f930abb90fa14f14691b56f
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-01-08 14:05:15 +0000

    TASK02: Modify fast_guess.sh
    Added some comments, improved program output a bit.
    
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task2-bash/fast_guess.sh
+++ task2-bash/fast_guess.sh
@@ -1,44 +1,47 @@
 #!/usr/bin/env bash
 
+# Shell option that allows the shell to recognize various extended pattern matching operators
+shopt -s extglob
+
+# Program name and initialize score
+PROG_NAME='guess'
 score=0
+
+# Compile the simple program
+gcc -w ../task1-simple-program/"$PROG_NAME".c -o "$PROG_NAME"
+
+# Calculating points and printing messages
 function score_update ()
 {
     if [[ $? == 0 ]]; then
         score=$((score+1))
-        echo "Good job"
+        echo "Good job!"
     else
-        echo "Wish a good luck next time"
+        echo "Wish good luck next time"
     fi
-    echo "$score"
+    echo "Number of success case(s): $score"
+    echo "======="
 }
 
+# Iterate over the number of trials
 function loop ()
 {
-    for (( c=1; c<="$1"; c++ ))
+    for ((c=1; c<="$1"; c++))
     do 
         ./"$PROG_NAME"
         score_update
     done
 }
 
-# Program name
-PROG_NAME='guess'
-
-# Compile the simple program
-gcc ../task1-simple-program/"$PROG_NAME".c -o "$PROG_NAME"
-
-# Shell option that allows the shell to recognize various extended pattern matching operators
-shopt -s extglob
-
-
+# Ask the user to guess
 while [ true ]
 do
 	echo "Press 'y' to continue, 'n' to exit. Or press number to set number of runs of tries."
 	read Variable
     case "$Variable" in
-        y        ) ./"$PROG_NAME"; score_update;;
-        n        ) break;;
+        y | Y    ) ./"$PROG_NAME"; score_update;;
+        n | N    ) break;;
         +([0-9]) ) loop Variable; break;;
-        *        ) echo "Sorry, please try one more time."
+        *        ) echo "Sorry, please try one more time.";;
     esac
 done
