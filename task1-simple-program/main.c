4489fadca09e4654b2f3cc472054fda7d0cd45f3
Serhii Shramko <shram.serhiy@gmail.com>
2023-01-08 15:41:07 +0200

    Task 1: Implement guess number game
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main(void)
 {
-	return 0;
+	srand(time(NULL));
+	puts("Enter number between 0 and 9");
+
+	int input_number;
+
+	scanf("%d", &input_number);
+	if (input_number == rand() % 10) {
+		puts("You win");
+		return 0;
+	}
+	puts("You loose");
+	return 1;
 }
