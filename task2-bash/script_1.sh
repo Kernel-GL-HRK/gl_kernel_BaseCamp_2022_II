65aebdd39b517da8b1be244ea507bee4c980bfd5
Sergiy Us <sergiy.us@gmail.com>
2023-01-08 12:37:28 +0200

    TASK02: Add copy gzip archive to release
    Added copy gzip archive to "release" subdirectory in project dir.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

--- task2-bash/script_1.sh
+++ task2-bash/script_1.sh
@@ -3,6 +3,7 @@
 
 path_source=~/gl_base_camp/gl_kernel_BaseCamp_2022_II/task1-simple-program
 path_destination=/tmp/guesanumber
+subdir=release
 
 if [ -d $path_destination ]
 then
@@ -16,3 +17,6 @@ cp $path_source/* ./
 
 destination_name=${PWD##*/}
 tar -czf $destination_name.tar.gzip ./*
+
+mkdir -p $path_source/${subdir}
+cp $path_destination/$destination_name.tar.gzip $path_source/${subdir}
