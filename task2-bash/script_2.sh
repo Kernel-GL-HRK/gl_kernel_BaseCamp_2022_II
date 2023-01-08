302bc4a8fd5d9dec7791151655362ee71011f8f3
Sergiy Us <sergiy.us@gmail.com>
2023-01-08 14:38:51 +0200

    TASK02-2: Add run program "guess a number"
    Added run program "guess a number" from task1 in infinity loop.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

--- task2-bash/script_2.sh
+++ task2-bash/script_2.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-# script_2.sh
 # guess a number
 
 loop=1
+game=~/gl_base_camp/gl_kernel_BaseCamp_2022_II/task1-simple-program/main
 
 while [ 1 ]
 do
+        for (( i = 0; i < loop; i++ ))
+        do
+                $game
+        done
+
         echo "To continue press \"y\", \"n\" or number of tries"
         read var
         if [[ $var = "y" ]]
         then
-                echo "Infinity loop test"
+                loop=1
         elif [[ $var = "n" ]]
         then
                 break
         elif [[ "$var" =~ ^[0-9]+$ ]]
         then
                 loop=$var
-                echo "number = $loop"
         else
+                loop=0
                 echo "Error input!"
         fi
 done
