b22317276677e8de78837337cce14011d61fc17c
Serhii Shramko <shram.serhiy@gmail.com>
2023-01-08 18:21:14 +0200

    Task 2.2: Add success case count
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>

--- task2-bash/script2.sh
+++ task2-bash/script2.sh
@@ -2,14 +2,19 @@
 
 GAME=../a.out
 loop_count=1
+sucess_count=0
 
 while true; do
     ./$GAME
     if [ $? -eq 0 ]; then
         echo "Good job"$'\n'
+        ((sucess_count++))
     else
         echo "Wish a good luck next time"$'\n'
     fi
+
+    echo "Correctly guessed $sucess_count times"$'\n'
+    
     ((loop_count--))
     if [ $loop_count -lt 1 ]; then
         read -p "Continue y/n. Or number of loops: " answ
