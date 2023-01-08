631eb95fc8acaaca844e6b85622a8598fc050d5e
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-01-08 11:49:50 +0000

    TASK02: Modify fast_guess.sh
    Added an infinite loop with a case expression. For extended pattern
    matching, the extglob shell option is used.
    
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task2-bash/fast_guess.sh
+++ task2-bash/fast_guess.sh
@@ -6,5 +6,16 @@ PROG_NAME='guess'
 # Compile the simple program
 gcc ../task1-simple-program/"$PROG_NAME".c -o "$PROG_NAME"
 
-# Test
-./"$PROG_NAME"
+# Shell option that allows the shell to recognize various extended pattern matching operators
+shopt -s extglob
+
+while [ true ]
+do
+	echo "Press 'y' to continue, 'n' to exit. Or press number to set number of runs of tries."
+	read Variable
+    case "$Variable" in
+        y        ) ./"$PROG_NAME";;
+        n        ) break;;
+        +([0-9]) ) echo "Loop"; break;;
+    esac
+done
