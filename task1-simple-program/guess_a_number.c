51ef3d9f14ee300076cc49b3da02af6f2ce8e1ea
Laura Shcherbak <45738929+laurashcherbak@users.noreply.github.com>
2023-01-08 22:54:20 +0200

    Homework 1: Add simple game "guess a number" (#28)
    * Homeworks 1. Create readme file
    
    Signed-off-by: Oleksii Vodka <oleksii.vodka@gmail.com>
    
    * Homeworks 2: Create readme file
    
    Signed-off-by: Oleksii Vodka <oleksii.vodka@gmail.com>
    
    * Homeworks 3: Create readme file
    
    Signed-off-by: Oleksii Vodka <oleksii.vodka@gmail.com>
    
    * Homework 1: Add .gitignore
    
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>
    
    * Homework 1-3: Add checkpatch.pl
    
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>
    
    * Homework 1: Create simple *.c file with empty main() function
    
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>
    
    * Homework 1: Write main functionality
    
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>
    
    * Homework 1: Rewrite code, that random number generates in my own defined function
    
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>
    
    * Homework 2: Update .gitignore
    
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>
    
    Signed-off-by: Oleksii Vodka <oleksii.vodka@gmail.com>
    Signed-off-by: Laura Shcherbak <laura.shcherbak@gmail.com>
    Co-authored-by: Oleksii Vodka <oleksii.vodka@gmail.com>

---
+++ task1-simple-program/guess_a_number.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+int get_computer_number(void)
+{
+	srand(time(NULL));
+	return (rand() % 10);
+}
+
+int main(void)
+{
+	int user_number, computer_number;
+	bool result;
+
+	printf("Enter your number from 0 to 9: ");
+	scanf("%d", &user_number);
+
+	if (user_number >= 0 && user_number <= 9) {
+		computer_number = get_computer_number();
+
+		//printf("Computer number: %d\n", computer_number);
+
+		result = (user_number == computer_number);
+		printf("You %s\n", result ? "win" : "loose");
+	} else {
+		printf("incorrect number\n");
+		result = 0;
+	}
+
+	return !(int)(result);
+}
