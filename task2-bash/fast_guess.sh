9b033617616622d2176ddd7ac9932b028a24f7e2
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-01-08 12:38:23 +0000

    TASK02: Add function to fast_guess.sh file
    Added the function of counting score and printing messages.
    
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task2-bash/fast_guess.sh
+++ task2-bash/fast_guess.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+score=0
+function score_update ()
+{
+    if [[ $? == 0 ]]; then
+        score=$((score+1))
+        echo "Good job"
+    else
+        echo "Wish a good luck next time"
+    fi
+}
+
 # Program name
 PROG_NAME='guess'
 
@@ -9,13 +20,16 @@ gcc ../task1-simple-program/"$PROG_NAME".c -o "$PROG_NAME"
 # Shell option that allows the shell to recognize various extended pattern matching operators
 shopt -s extglob
 
+
 while [ true ]
 do
 	echo "Press 'y' to continue, 'n' to exit. Or press number to set number of runs of tries."
 	read Variable
     case "$Variable" in
-        y        ) ./"$PROG_NAME";;
+        y        ) ./"$PROG_NAME"; score_update;;
         n        ) break;;
         +([0-9]) ) echo "Loop"; break;;
+        *        ) echo "Sorry, please try one more time."
     esac
+    echo "$score"
 done
