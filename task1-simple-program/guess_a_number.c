4f27000adf038a2849cfa8d141db3dc24a0ce2f4
Semen Bazyliuk <bazsem2000@gmail.com>
2023-01-09 11:46:38 +0200

    TASK01: Add main functionality to game
    Added main functionality to guess_a_number.c
    Now code generates random number. Gets user input and check if it valid (in range and a number).
    Then prompt massage based on if user guess number corectly or not
    
    Signed-off-by: Semen Bazyliuk <bazsem2000@gmail.com>

--- task1-simple-program/guess_a_number.c
+++ task1-simple-program/guess_a_number.c
@@ -1,8 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
 
 int main(void)
 {
-	return 0;
+	int input_num, rand_num;
+
+	srand(time(NULL));
+	rand_num = rand() % 10;
+	printf("Guess a number from 0 to 9\n");
+
+	//Uncomment for game debugging
+	printf("rand_num: %d\n", rand_num);
+
+	while (1) {
+		printf("Enter your guess: ");
+		if (!(scanf("%d", &input_num) > 0)) {
+			printf("Not number at all. Try again!\n");
+			getchar();//flush input stdint buffer to prevent infinite scanf reading
+		} else {
+			if (!(input_num >= 0 && input_num <= 9))
+				printf("Number is out of range [0-9]. Try again!\n");
+			else
+				break;
+		}
+	}
+
+	if (rand_num == input_num)
+		printf("You win!\n");
+	else
+		printf("You loose(\nThe number was %d\n", rand_num);
+
+	return 1;
 }
