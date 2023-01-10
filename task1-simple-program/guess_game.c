9937aec93797a2a70e74202f26a45d37b4074c52
EugeneKolesnyk <kolesnyk4996@gmail.com>
2023-01-10 11:26:07 +0200

    Task1: Modify the main function in the guess_game.c
    Signed-off-by: EugeneKolesnyk <kolesnyk4996@gmail.com>

--- task1-simple-program/guess_game.c
+++ task1-simple-program/guess_game.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]){
+	srand(time(NULL));
+	uint8_t randNum = rand() % 10;
+	uint8_t userNum = 0;
+
+	if(argc > 1) userNum = atoi(argv[1]);
+
+	else {
+		printf("Enter your number (0-9): ");
+		scanf("%hhu", &userNum); 
+	}
+
+	if (userNum != randNum){
+		printf("You loose (%hu - %hu)\n", userNum, randNum);
+		return -1;
+	}
+
+	printf("You win (%hu - %hu)\n", userNum, randNum);
 	return 0;
-)
+}
