1205530184e35ae0c3212653b656bf32b3d36dcb
Dmytro Serhieiev <sdv04022000@gmail.com>
2023-01-11 00:45:57 +0200

    Task1: Add main functionality to guess_a_number.c
    Now the main() function generates a random number from
    0 to 9, reads the number entered by the user, checks
    if it is valid and displays a message depending on
    whether the user guessed the number or not.
    
    Signed-off-by: Dmytro Serhieiev <sdv04022000@gmail.com>

--- task1-simple-program/guess_a_number.c
+++ task1-simple-program/guess_a_number.c
@@ -2,8 +2,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define MIN_VALUE 0
+#define MAX_VALUE 9
 
 int main(void)
 {
-	return 0;
+	int user_number, random_number;
+
+	srand((unsigned int)time(NULL));
+	random_number = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
+
+	printf("Welcome to game \"guess a number\"!\n");
+	printf("Enter number from %d to %d: ", MIN_VALUE, MAX_VALUE);
+	scanf("%d", &user_number);
+	while (user_number < MIN_VALUE || user_number > MAX_VALUE) {
+		printf("Invalid value. Number must be from %d to %d. Try again: ", MIN_VALUE, MAX_VALUE);
+		scanf("%d", &user_number);
+	}
+
+	if (user_number == random_number) {
+		printf("You win\n");
+		return 0;
+	}
+
+	printf("You loose\n");
+	return 1;
 }
