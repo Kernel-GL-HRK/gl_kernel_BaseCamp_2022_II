3868ee42a24833ff97fa16390642045fc000c4ed
Vadym Minziuk <vadym.minziuk@gmail.com>
2023-01-11 06:40:26 +0200

    TASK02: Implement result announcement
    Script counts users’ success attempts and prints their number.
    After each success script says "Good job",
    otherwise "Wish a good luck next time".
    
    Signed-off-by: Vadym Minziuk <vadym.minziuk@gmail.com>

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+retcode=0
+wins=0
 count=1
+
+Color_Off='\033[0m' # Text Reset
+Red='\033[0;31m'    # Red
+Green='\033[0;32m'  # Green
+Yellow='\033[0;33m' # Yellow
 
 while [ "$count" -gt 0 ]
 do
 	./guesanumber
+	let retcode=$?
+
+	if [ "$retcode" -eq 0 ]; then
+		let wins=$wins+1
+		echo -e "${Green}Good job${Color_Off}"
+	else
+		echo -e "${Yellow}Wish a good luck next time${Color_Off}"
+	fi
+
+	echo "Number of success cases: ${wins}"
 done
