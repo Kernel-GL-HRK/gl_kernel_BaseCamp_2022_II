70fae2302baf69ad94cdd1db691d0f9e42da2334
Vadym Minziuk <vadym.minziuk@gmail.com>
2023-01-11 05:50:09 +0200

    TASK02: Implement colour announcement
    To inform user to run script in project directory
    
    Signed-off-by: Vadym Minziuk <vadym.minziuk@gmail.com>

---
+++ archiver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Color_Off='\033[0m' # Text Reset
+Red='\033[0;31m'    # Red
+On_White='\033[47m' # White
+
+echo -e "${Red}${On_White}To archive source code${Color_Off}"
+echo -e "${Red}${On_White}run this script from the project directory${Color_Off}"
