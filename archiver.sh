42f64137d2ffd867a1b9a5429ee5257a175b54a6
Vadym Minziuk <vadym.minziuk@gmail.com>
2023-01-11 06:01:27 +0200

    TASK02: Implement copying of source code
    Copying all source code from the project to /tmp/guesanumber
    
    Signed-off-by: Vadym Minziuk <vadym.minziuk@gmail.com>

--- archiver.sh
+++ archiver.sh
@@ -6,3 +6,8 @@ On_White='\033[47m' # White
 
 echo -e "${Red}${On_White}To archive source code${Color_Off}"
 echo -e "${Red}${On_White}run this script from the project directory${Color_Off}"
+
+# Copy source code
+rm -rf /tmp/guesanumber 2>/dev/null
+mkdir /tmp/guesanumber
+cp --archive ./*.c ./*.h ./*.md ./*.sh /tmp/guesanumber 2>/dev/null
