c390aa7e7e02a7966aa18388a1af8f01190729e1
Dmytro Serhieiev <sdv04022000@gmail.com>
2023-01-13 22:11:38 +0200

    Task2: Implement parameter checking in arch_project.sh
    The script uses positional parameters to define the path
    to the project directory and the name of the target archive.
    If the script is run without passing parameters, they are
    assigned default values.
    
    Signed-off-by: Dmytro Serhieiev <sdv04022000@gmail.com>

--- task2-bash/arch_project.sh
+++ task2-bash/arch_project.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+# Parameters:
+# $1 is the path to the project directory
+# $2 is the name of the target archive
+
+DEFAULT_DIRECTORY="../task1-simple-program"
+DEFAULT_ARCH_NAME="guesanumber"
+
+if [ $# -lt 1 ]; then
+    echo "Warning: Arguments are not specified. Using default values."
+    set $DEFAULT_DIRECTORY $DEFAULT_ARCH_NAME
+fi
+
+if [ $# -lt 2 ]; then
+    echo "Warning: Archive name is not specified. Using default value."
+    set $1 $DEFAULT_ARCH_NAME
+fi
+
+if [ ! -d $1 ]; then
+    echo "Error: $1 is not a directory."
+    exit 1
+fi
