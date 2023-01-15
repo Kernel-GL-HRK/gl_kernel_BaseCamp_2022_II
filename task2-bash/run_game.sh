6e077189679abd35857c8cebc22715b9c6b30646
Dmytro Serhieiev <sdv04022000@gmail.com>
2023-01-15 17:45:43 +0200

    Task2: Add launching the game in an infinite loop
    Added an infinite loop in which the game "guess a number"
    is launched. At each iteration there is an option to exit
    the loop or continue.
    
    Signed-off-by: Dmytro Serhieiev <sdv04022000@gmail.com>

--- task2-bash/run_game.sh
+++ task2-bash/run_game.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# Path to game "guess a number":
+GAME="../task1-simple-program/guess_a_number.out"
+
+while true; do
+
+    echo "Enter Y to continue or N to exit:"
+    read reply
+    case $reply in
+        Y|y)    ;;
+        N|n)    break ;;
+        *)      echo "Invalid choice!"
+                continue ;;
+    esac
+
+    $GAME
+    
+done
