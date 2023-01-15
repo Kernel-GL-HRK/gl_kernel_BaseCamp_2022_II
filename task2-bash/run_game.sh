e6090179d4823b6724523768b0fc0b664a735123
Dmytro Serhieiev <sdv04022000@gmail.com>
2023-01-15 18:01:43 +0200

    Task2: Implement number input processing in run_game.sh
    The user can now enter the number of attempts he wants
    to play without interruption.
    
    Signed-off-by: Dmytro Serhieiev <sdv04022000@gmail.com>

--- task2-bash/run_game.sh
+++ task2-bash/run_game.sh
@@ -3,16 +3,28 @@
 # Path to game "guess a number":
 GAME="../task1-simple-program/guess_a_number.out"
 
+# Variables:
+attempt_cnt=0
+
+# Command to enable extended pattern matching:
+shopt -s extglob
+
 while true; do
 
-    echo "Enter Y to continue or N to exit:"
-    read reply
-    case $reply in
-        Y|y)    ;;
-        N|n)    break ;;
-        *)      echo "Invalid choice!"
-                continue ;;
-    esac
+    if [ $attempt_cnt -eq 0 ]; then
+        echo "Enter Y to continue, N to exit, or the number of"
+        echo "attempts you want to play without interruption:"
+        read reply
+        case $reply in
+            Y|y)        ;;
+            N|n)        break ;;
+            +([0-9]))   let attempt_cnt=reply-1 ;;
+            *)          echo "Invalid choice!"
+                        continue ;;
+        esac
+    else 
+        let attempt_cnt--
+    fi
 
     $GAME
     
