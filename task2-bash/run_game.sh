04438a4f493200158519a379172d95ca9a8f5fca
Dmytro Serhieiev <sdv04022000@gmail.com>
2023-01-15 18:51:23 +0200

    Task2: Implement colored output in run_game.sh
    Signed-off-by: Dmytro Serhieiev <sdv04022000@gmail.com>

--- task2-bash/run_game.sh
+++ task2-bash/run_game.sh
@@ -3,6 +3,12 @@
 # Path to game "guess a number":
 GAME="../task1-simple-program/guess_a_number.out"
 
+# Colors:
+red="\e[1;31m"
+green="\e[1;32m"
+yellow="\e[1;33m"
+reset="\e[m"
+
 # Variables:
 attempt_cnt=0
 win_cnt=0
@@ -13,14 +19,14 @@ shopt -s extglob
 while true; do
 
     if [ $attempt_cnt -eq 0 ]; then
-        echo "Enter Y to continue, N to exit, or the number of"
-        echo "attempts you want to play without interruption:"
+        echo -e "${yellow}Enter Y to continue, N to exit, or the number of${reset}"
+        echo -e "${yellow}attempts you want to play without interruption:${reset}"
         read reply
         case $reply in
             Y|y)        ;;
             N|n)        break ;;
             +([0-9]))   let attempt_cnt=reply-1 ;;
-            *)          echo "Invalid choice!"
+            *)          echo -e "${red}Invalid choice!${reset}"
                         continue ;;
         esac
     else 
@@ -31,11 +37,11 @@ while true; do
 
     if [ $? -eq 0 ]; then
         let win_cnt++
-        echo "Good job!"
+        echo -e "${green}Good job!${reset}"
     else
-        echo "Wish a good luck next time."
+        echo -e "${red}Wish a good luck next time.${reset}"
     fi
 
-    echo "You have won $win_cnt time(s)."
+    echo -e "${yellow}You have won $win_cnt time(s).${reset}"
     
 done
