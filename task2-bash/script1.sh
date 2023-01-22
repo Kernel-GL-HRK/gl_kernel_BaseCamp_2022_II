b1b91f7d21d4e9024183c435056e234ac4ba7f8b
serhii-shramko <99900349+serhii-shramko@users.noreply.github.com>
2023-01-22 16:25:34 +0200

    Task 2: Bash (#55)
    * Task 2.1: Add empty script file
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.1: Add copy functionality
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.1: Add archive functionality
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.1: Add copy to release functionality
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.2: Add empty script file
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.2: Add infinite loop without exit
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.2: Add message after game
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.2: Add exit from loop
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    * Task 2.2: Add success case count
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>
    
    Signed-off-by: Serhii Shramko <shram.serhiy@gmail.com>

---
+++ task2-bash/script1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT_PATH=../task1-simple-program
+DIR_PATH=/tmp/guesanumber
+
+if [ ! -d "$DIR_PATH" ]; then
+    mkdir $DIR_PATH
+fi
+
+cp $PROJECT_PATH/*.c $DIR_PATH
+
+tar cvzf /tmp/guesanumber.tar.gz -C /tmp guesanumber 
+
+if [ ! -d "release" ]; then
+    mkdir release
+fi
+
+cp /tmp/guesanumber.tar.gz release/
