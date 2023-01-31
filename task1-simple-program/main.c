099f7e109f3d0e140258d5df0f2a213945dcc460
Roman Muravka <roman.muravka@nure.ua>
2023-01-31 14:31:47 +0200

    Task_1.4: Change: main.c(checkpatch.pl)
    Optimized main.c by checkpatch.pl for better style and it better work.
    
    Signed-off-by: Roman Muravka <roman.muravka@nure.ua>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -3,25 +3,26 @@
 #include <stdint.h>
 #include <time.h>
 
-uint16_t randomize()
+uint16_t randomize(void)
 {
   srand(time(NULL));
   return (rand() % 10);
-  
+
 }
 
-int main() {
+int main() 
+{
   uint16_t UserNum = 0xFFFF, QuessNum = 0xFFFF;
+
   printf("Please enter number from 0 to 9: ");
-  scanf("%d", &UserNum);
+  scanf("%hd", &UserNum);
   if (UserNum <= 9) {
     QuessNum = randomize();
-    if (UserNum == QuessNum){
-      printf("!!!You win!!!");
+    if (UserNum == QuessNum) {
+      printf("!!!You win!!!\n");
       return 0;
-    }
-    else{
-      printf("!!!You lose!!! - Quess Number was: %d", QuessNum);
+    } else{
+      printf("!!!You lose!!! - Quess Number was: %hd\n", QuessNum);
       return 1;
     }
   }
