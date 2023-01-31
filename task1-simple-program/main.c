e0b8798b01444d518617d3e0a5456383b8d6c3fe
Roman Muravka <roman.muravka@nure.ua>
2023-01-31 14:23:24 +0200

    Task_1.3 - Change: main.c(randomize())
    rewrote main.c file, highlighting the commands for generating random numbers in a separate function.
    
    Signed-off-by: Roman Muravka <roman.muravka@nure.ua>

--- task1-simple-program/main.c
+++ task1-simple-program/main.c
@@ -3,16 +3,26 @@
 #include <stdint.h>
 #include <time.h>
 
-int main() {
-  uint16_t UserNum = 0xFF, QuessNum = 0xFF;
+uint16_t randomize()
+{
   srand(time(NULL));
+  return (rand() % 10);
+  
+}
+
+int main() {
+  uint16_t UserNum = 0xFFFF, QuessNum = 0xFFFF;
   printf("Please enter number from 0 to 9: ");
   scanf("%d", &UserNum);
   if (UserNum <= 9) {
-    QuessNum = rand() % 10;
-    if (UserNum == QuessNum)
+    QuessNum = randomize();
+    if (UserNum == QuessNum){
       printf("!!!You win!!!");
-    else
+      return 0;
+    }
+    else{
       printf("!!!You lose!!! - Quess Number was: %d", QuessNum);
+      return 1;
+    }
   }
-} 
+}
