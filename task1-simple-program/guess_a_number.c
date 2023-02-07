dfff31cf8ae242a8ee644f897a06f265e2d5e13f
G2p1 <andrew.tiuitunnyk@gmail.com>
2023-02-07 11:40:16 +0200

    Task1: changed main() function to add game functionality
    Signed-off-by: G2p1 <andrew.tiuitunnyk@gmail.com>

--- task1-simple-program/guess_a_number.c
+++ task1-simple-program/guess_a_number.c
@@ -1,6 +1,21 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-	return 0;
+	srand(time(NULL));
+	int randomNumber = rand()%9;
+
+	int userNumber, result;
+	result = scanf("%d", &userNumber);
+
+	if(userNumber == randomNumber){
+		printf("You win\n");
+		return 0;
+	}
+	else{
+		printf("You loose\n");
+		return 4;
+	}
 }
