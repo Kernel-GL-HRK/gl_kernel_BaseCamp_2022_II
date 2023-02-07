07817446aa7eef2a79c3cc3ac0d3e715387ad3fe
G2p1 <andrew.tiuitunnyk@gmail.com>
2023-02-07 22:09:59 +0200

    Task3: add file with main function where used dl
    Signed-off-by: G2p1 <andrew.tiuitunnyk@gmail.com>

---
+++ task3-make/guess_a_number_dll.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main()
+{
+	
+	int userNumber, result;
+	result = scanf("%d", &userNumber);
+	
+	void *handle = dlopen("librandomeGenerate.so", RTLD_LAZY);
+	if(!handle){
+		printf("error with load dynamic library");
+	}
+	void (*lib_generate)();
+	lib_generate = (void (*)dlsym(handle), "randomGenerate");
+	if(dlerror() != NULL){
+		printf("error");
+	}
+
+	if(userNumber == (*lib_generate)()){
+		printf("You win\n");
+		return 0;
+	}
+	else{
+		printf("You loose\n");
+		return 4;
+	}
+	dlclose(handle);
+}
