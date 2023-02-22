cbf1407bc0f0b5868dda89faf134f4936374990d
kmakhno <kostiantynmakhno@gmail.com>
2023-02-22 10:22:46 +0200

    Kernel HW1: Fix header naming error
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- kernel_hw_1/hello.c
+++ kernel_hw_1/hello.c
@@ -1,15 +1,26 @@
-#include <kernel/init.h>
-#include <kernel/module.h>
-#include <kernel/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 
-MOUDLE_LICENSE("GPL");
+MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple module");
 MODULE_AUTHOR("Kostiantyn Makhno");
 MODULE_VERSION("0.1");
 
+static int op1;
+static int op2;
+
+module_param(op1, int, 0444);
+MODULE_PARM_DESC(op1, "The first integer operand");
+module_param(op2, int, 0444);
+MODULE_PARM_DESC(op2, "The second integer operand");
+
 static int __init hello_init(void)
 {
 	pr_info("hello module was initialized\n");
+	pr_info("sum of operands: %d\n", op1 + op2);
+	pr_info("diff of operands: %d\n", op1 - op2);
+	pr_info("mult of operands: %d\n", op1 * op2);
 
 	return 0;
 }
