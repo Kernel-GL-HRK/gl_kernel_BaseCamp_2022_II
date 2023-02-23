8ba22efe35c49e878bfda0398000616451e29b7e
Sergiy Us <sergiy.us@gmail.com>
2023-02-23 22:18:34 +0200

    TASK04: Add simple calculator
    A simple calculator has been added to this kernel module.
    This module accepts two variables par1 and par2 and prints the sum,
    subtraction (sub), multiplication (mul) of these parameters.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

--- task4-hello-world-module/hello.c
+++ task4-hello-world-module/hello.c
@@ -2,15 +2,30 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sergiy Us");
-MODULE_DESCRIPTION("Hello kernel module");
+MODULE_DESCRIPTION("The simple calculator");
 MODULE_VERSION("0.1");
+
+static long par1;
+static long par2;
+
+module_param(par1, long, 0444);
+MODULE_PARM_DESC(par1, "The first variable");
+
+module_param(par2, long, 0444);
+MODULE_PARM_DESC(par2, "The second variable");
 
 static int __init hello_init(void)
 {
 	pr_info("HELLO: Hello linux kernel!\n");
+	pr_info("CALC: par1 = %ld, par2 = %ld\n", par1, par2);
+	pr_info("CALC: sum = %ld\n", par1 + par2);
+	pr_info("CALC: sub = %ld\n", par1 - par2);
+	pr_info("CALC: mul = %ld\n", par1 * par2);
 	return 0;
 }
 
