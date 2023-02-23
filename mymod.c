31efff72838e84ad22355ba4146ae747172c8a6c
yurakl <yuraklisko@gmail.com>
2023-02-23 11:29:29 +0200

    TASK04: Add parameters and math ops in mymod.c

--- mymod.c
+++ mymod.c
@@ -5,13 +5,25 @@
 
 #define DEVICE_NAME "mymod"
 
-static int __init init_function(void) {
-	pr_info("%s: %s - %s:%d\n", DEVICE_NAME, "INIT", __FUNCTION__, __LINE__);
+static int num_1, num_2;
+module_param(num_1, int, 0400);
+module_param(num_2, int, 0400);
+
+MODULE_PARM_DESC(num_1, "1st value");
+MODULE_PARM_DESC(num_2, "2nd value");
+
+static int __init init_function(void)
+{
+	pr_info("%s: %s - %s:%d\n", DEVICE_NAME, "INIT", __func__, __LINE__);
+	pr_info("%s: %d + %d = %d\n", DEVICE_NAME, num_1, num_2, num_1 + num_2);
+	pr_info("%s: %d - %d = %d\n", DEVICE_NAME, num_1, num_2, num_1 - num_2);
+	pr_info("%s: %d * %d = %d\n", DEVICE_NAME, num_1, num_2, num_1 * num_2);
 	return 0;
 }
 
-static void __exit exit_function(void) {
-	pr_info("%s: %s - %s:%d\n", DEVICE_NAME, "EXIT", __FUNCTION__, __LINE__);
+static void __exit exit_function(void)
+{
+	pr_info("%s: %s - %s:%d\n", DEVICE_NAME, "EXIT", __func__, __LINE__);
 }
 
 
