c8d5f41530da1c50f56cbdec65e34a0c101a6c6f
EugeneKolesnyk <kolesnyk4996@gmail.com>
2023-02-24 00:09:53 +0200

    Hello_module: Fix hello.c according to checkpatch.pl
    Signed-off-by: EugeneKolesnyk <kolesnyk4996@gmail.com>

--- task4-module/hello.c
+++ task4-module/hello.c
@@ -9,23 +9,25 @@ MODULE_AUTHOR("Yevhen Kolesnyk");       ///< The author -- visible when you use 
 MODULE_DESCRIPTION("My first module");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");                  ///< The version of the module
 
-static int num1 = 0;
-static int num2 = 0;
+static int num1;
+static int num2;
 
-module_param(num1, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-module_param(num2, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+module_param(num1, int, 0660);
+module_param(num2, int, 0660);
 
 
-static int __init hello_init(void){
-    pr_info("HELLO: Hello Kernel!\n");
-    pr_info("HELLO: %d + %d = %d\n", num1, num2, num1 + num2);
-    pr_info("HELLO: %d - %d = %d\n", num1, num2, num1 - num2);
-    pr_info("HELLO: %d * %d = %d\n", num1, num2, num1 * num2);
-    return 0;
+static int __init hello_init(void)
+{
+pr_info("HELLO: Hello Kernel!\n");
+pr_info("HELLO: %d + %d = %d\n", num1, num2, num1 + num2);
+pr_info("HELLO: %d - %d = %d\n", num1, num2, num1 - num2);
+pr_info("HELLO: %d * %d = %d\n", num1, num2, num1 * num2);
+return 0;
 }
 
-static void __exit hello_exit(void){
-    pr_info("HELLO: Goodbye Kernel!\n");
+static void __exit hello_exit(void)
+{
+pr_info("HELLO: Goodbye Kernel!\n");
 }
 
 module_init(hello_init);
