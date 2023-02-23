fe59fc40b4acee2f0e8e9d3a39172a1ce79c3fc5
Maksym Khomenko <mr.homax@gmail.com>
2023-02-23 23:16:36 +0200

    Homework task4 simple driver: Refactor hello.c to satisfy the requirements of checkpatch.pl
    Signed-off-by: Maksym Khomenko <mr.homax@gmail.com>

--- task4-simple-driver/hello.c
+++ task4-simple-driver/hello.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 #include <linux/init.h> // Macros used to mark up functions e.g., __init __exit
 #include <linux/module.h> // Core header for loading LKMs into the kernel
 #include <linux/kernel.h> // Contains types, macros, functions for the kernel
@@ -7,16 +8,17 @@ MODULE_AUTHOR("Name Surname"); ///< The author -- visible when you use modinfo
 MODULE_DESCRIPTION("Description"); ///< The description -- see modinfo
 MODULE_VERSION("0.1"); ///< The version of the module
 
-static int op1 = 0;
-static int op2 = 0;
+static int op1 = -1;
+static int op2 = -1;
 
-module_param(op1, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+module_param(op1, int, 0660);
 MODULE_PARM_DESC(op1, "First parameter of type s32");
 
-module_param(op2, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+module_param(op2, int, 0660);
 MODULE_PARM_DESC(op2, "Second parameter of type s32");
 
-static int __init hello_init(void){
+static int __init hello_init(void)
+{
 pr_info("HELLO: Hello Kernel!\n");
 pr_info("HELLO: Sum of parameters = %d\n", op1 + op2);
 pr_info("HELLO: Difference of parameters = %d\n", op1 - op2);
@@ -24,7 +26,8 @@ pr_info("HELLO: Product of parameters = %d\n", op1 * op2);
 return 0;
 }
 
-static void __exit hello_exit(void){
+static void __exit hello_exit(void)
+{
 pr_info("HELLO: Goodbye Kernel!\n");
 }
 
