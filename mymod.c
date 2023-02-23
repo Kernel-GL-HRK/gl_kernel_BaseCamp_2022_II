5fac488f4419aaa8dee115a64eadea1e9a3c3ad4
yurakl <yuraklisko@gmail.com>
2023-02-23 11:06:24 +0200

    TASK04: Add mymod.c and Makefile

---
+++ mymod.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/moduleparam.h>
+
+#define DEVICE_NAME "mymod"
+
+static int __init init_function(void) {
+	pr_info("%s: %s - %s:%d\n", DEVICE_NAME, "INIT", __FUNCTION__, __LINE__);
+	return 0;
+}
+
+static void __exit exit_function(void) {
+	pr_info("%s: %s - %s:%d\n", DEVICE_NAME, "EXIT", __FUNCTION__, __LINE__);
+}
+
+
+module_init(init_function);
+module_exit(exit_function);
+
+MODULE_AUTHOR("Yurii Klysko");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simle kernel module.");
