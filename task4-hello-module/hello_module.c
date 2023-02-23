2a96d2b055e416f9677eafc92fe2b74328e6f17c
Viacheslav Us <slava.us@icloud.com>
2023-02-23 22:09:05 +0200

    TASK04: Implement simple calculator
    Implemented sum, subtraction and multiplication of two parameters
    
    Signed-off-by: Viacheslav Us <slava.us@icloud.com>

--- task4-hello-module/hello_module.c
+++ task4-hello-module/hello_module.c
@@ -2,16 +2,28 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/init/h>
+#include <linux/init.h>
+#include <linux/types.h>
+#include <linux/moduleparam.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Viacheslav Us");
 MODULE_DESCRIPTION("First hello kernel module");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
+
+static int num1;
+static int num2;
+
+module_param(num1, int, 0444);
+module_param(num2, int, 0444);
 
 static int __init hello_init(void)
 {
 	pr_info("Hello World!\n");
+
+	pr_info("%d + %d = %d\n", num1, num2, num1 + num2);
+	pr_info("%d - %d = %d\n", num1, num2, num1 - num2);
+	pr_info("%d * %d = %d\n", num1, num2, num1 * num2);
 
 	return 0;
 }
