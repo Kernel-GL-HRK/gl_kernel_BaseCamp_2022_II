2ac8868dcddc6a8b3ece7911011a4fcf3f64d9f8
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-02-24 01:08:41 +0000

    TASK04: Add licensing and module documentation
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task4-kernel-modules/hello_world.c
+++ task4-kernel-modules/hello_world.c
@@ -2,6 +2,10 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Andrii");
+MODULE_DESCRIPTION("A sample driver");
+
 static int __init hello_init(void)
 {
 	pr_info("Hello world.\n");
