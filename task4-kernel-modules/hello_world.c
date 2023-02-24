52cc7f2c99dc4beeff89cee43809e99c4474ae7b
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-02-24 00:59:31 +0000

    TASK04: Change hello_world.c with macros
    Add module_init() and module_exit() macros.
    
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task4-kernel-modules/hello_world.c
+++ task4-kernel-modules/hello_world.c
@@ -1,13 +1,17 @@
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 
-int init_module(void)
+static int __init hello_init(void)
 {
 	pr_info("Hello world.\n");
 	return 0;
 }
 
-void cleanup_module(void)
+static void __exit hello_exit(void)
 {
 	pr_info("Goodbye world.\n");
 }
+
+module_init(hello_init);
+module_exit(hello_exit);
