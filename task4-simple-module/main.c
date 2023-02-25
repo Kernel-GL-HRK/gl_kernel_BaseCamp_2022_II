fb02063be1932bebcd66becb52f1a54b767b6e00
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-02-26 01:05:48 +0200

    Homework 4: Define pr_fmt
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task4-simple-module/main.c
+++ task4-simple-module/main.c
@@ -6,16 +6,21 @@ MODULE_DESCRIPTION("Simple kernel module");
 MODULE_AUTHOR("Tereshchenko Dmytro <buxdmo@gmail.com>");
 MODULE_VERSION("1.0");
 
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 /*===============================================================================================*/
 static int driver_init(void)
 {
-	pr_debug("module_init\n");
+	pr_notice("module_init\n");
 	return 0;
 }
 /*===============================================================================================*/
 static void driver_exit(void)
 {
-	pr_debug("module_exit\n");
+	pr_notice("module_exit\n");
 }
 
 /*===============================================================================================*/
