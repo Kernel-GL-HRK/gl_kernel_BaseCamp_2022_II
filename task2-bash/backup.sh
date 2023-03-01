7db8264394124e8030fcdbdcfa961a1690d659c1
Vlad Degtyarov <deesyync@gmail.com>
2023-03-01 13:35:44 +0000

    Task02: Change dir variable names
    - Changed variable names (indicating directories) to be more clear
    - Also wrapped the most of variables in the script with quotation marks
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>

--- task2-bash/backup.sh
+++ task2-bash/backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -ex
 
-src="../task1-simple-program"
-tmp="/tmp/guesanumber"
-release="$src/release"
+src_dir="../task1-simple-program"
+tmp_dir="/tmp/guesanumber"
+release_dir="${src_dir}/release"
 
 
 ## Flushing tmp directory
-[ -d $tmp ] && rm -f $tmp/*
+[ -d "$tmp_dir" ] && rm -f "${tmp_dir}/*"
 
 ## Copying files to tmp directory
-mkdir -p $tmp
-cp $(find $src -type f -name "*.[c,h]") $tmp
+mkdir -p "$tmp_dir"
+cp $(find ${src_dir} -type f -name "*.[c,h]") "$tmp_dir"
 
 ## Archiving backup to release directory
-mkdir -p $release
-tar --directory=$(dirname $tmp) -cvzf \
-	$release/$(basename $tmp).$(date "+%F.%H-%M-%S").tar.gz \
-	$(basename $tmp)
+mkdir -p "$release_dir"
+tar --directory=$(dirname "${tmp_dir}") -cvzf \
+	"${release_dir}/$(basename ${tmp_dir}).$(date "+%F.%H-%M-%S").tar.gz" \
+	"$(basename ${tmp_dir})"
