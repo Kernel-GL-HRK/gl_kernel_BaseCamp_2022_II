6ccf4c52d9e3c1d1ea308f0a9d368518902ef20c
Roman Muravka <roman.muravka@nure.ua>
2023-03-05 16:41:11 +0200

    Task_4: Change and test - hello.c(parameters); Add - hello.ko(module);
    Added operations on parameters into hello.c file for task(sum, substraction, multiplication).
    Builded hello.ko file as module for running in kernel.
    Program also was tested by checkpatch.pl.
    
    Signed-off-by: Roman Muravka <roman.muravka@nure.ua>

--- task4-kernel-module-hello/hello.c
+++ task4-kernel-module-hello/hello.c
@@ -1,14 +1,25 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/moduleparam.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Roman Muravka");
 MODULE_DESCRIPTION("Simple program which print \"Hello World\" into syslog");
 MODULE_VERSION("1.0");
 
+int num1 = 0, num2 = 0;
+module_param(num1, int, 0644);
+module_param(num2, int, 0644);
+
 static int hello_init(void){
+
     pr_info("HELLO: !!!Hello World!!!\n");
+    if (!(num1 == 0 && num2 == 0)){
+        pr_info("HELLO: param1{%d} + param2{%d} = %d\n", num1, num2, num1 + num2);
+        pr_info("HELLO: param1{%d} - param2{%d} = %d\n", num1, num2, num1 - num2);
+        pr_info("HELLO: param1{%d} * param2{%d} = %d\n", num1, num2, (num1 * num2));
+    }
     return 0;
 }
 
