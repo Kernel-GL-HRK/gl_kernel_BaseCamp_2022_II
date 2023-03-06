6ee4d402b39de9c51081f4965a596f1df78bbf8b
kmakhno <kostiantynmakhno@gmail.com>
2023-03-06 21:49:24 +0200

    Kernel HW2: Add timer functionality
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- kernel_hw_2/hello.c
+++ kernel_hw_2/hello.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/gpio.h>
@@ -8,9 +9,18 @@
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple led driver");
 
-#define LED_PIN 23UL 
+#define LED_PIN 23UL
+#define PERIOD  1000UL
 
 static struct timer_list tm;
+static unsigned int led_state;
+
+void tm_callback(struct timer_list *ptm)
+{
+	led_state = (~led_state) & 0x1;
+	gpio_set_value(LED_PIN, led_state);
+	mod_timer(&tm, jiffies + msecs_to_jiffies(PERIOD));
+}
 
 static int __init hello_init(void)
 {
@@ -23,14 +33,14 @@ static int __init hello_init(void)
 		pr_info("can not request %ld pin", LED_PIN);
 		goto err_req_gpio;
 	}
-	
-	if (gpio_direction_output(LED_PIN, 0) != 0) {
+
+	if (gpio_direction_output(LED_PIN, led_state) != 0) {
 		pr_info("can not set direction on %ld pin\n", LED_PIN);
 		goto err_set_dir_gpio;
 	}
 
-//	timer_setup(&tm, tm_callback, 0);
-//	mod_timer(&tm, jiffies + msecs_to_jiffies(TIMEOUT));
+	timer_setup(&tm, tm_callback, 0);
+	mod_timer(&tm, jiffies + msecs_to_jiffies(PERIOD));
 
 	return 0;
 
@@ -43,7 +53,7 @@ err_valid_gpio:
 
 static void __exit hello_exit(void)
 {
-//	del_timer(&tm);
+	del_timer(&tm);
 	gpio_set_value(LED_PIN, 0);
 	gpio_free(LED_PIN);
 	pr_info("led pin %ld was freed\n", LED_PIN);
