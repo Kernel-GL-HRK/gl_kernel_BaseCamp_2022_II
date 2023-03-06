19f755a21ad87e4b19cfd71b76fd15a2a616930e
yurakl <yuraklisko@gmail.com>
2023-03-06 12:26:43 +0200

    Task05: Edit code with checkpatch and comments

--- task05/mymod.c
+++ task05/mymod.c
@@ -8,34 +8,48 @@
 
 #define DEVICE_NAME "mymod"
 #define PROC_NAME "mymod"
+/* Permissions to the procfs file */
 #define PERMS 0644
+
+/* Buffer to send to userspace */
 #define BUFS 256
 static char modbuf[BUFS] = {0};
+
+/* proc_fs directory and file */
 static struct proc_dir_entry *proc_dir, *proc_file;
+
 static ssize_t mymod_read(struct file *filp, char __user *buf, size_t count, loff_t *pos);
 
+/* Time of lightning or sleeping (in ms) */
 #define PERIOD 5000
 static struct timer_list my_timer;
 
+/* GPIO, where LED is connected */
 #define LED 26
 
-void my_timer_callback(struct timer_list * this_timer)
+/*  Recursive timer handler, switch gpio level at each entry. */
+void my_timer_callback(struct timer_list *this_timer)
 {
 	static int level;
+
 	level = !level;
 	gpio_set_value(LED, level);
 	pr_info("%s: %s %d\n", DEVICE_NAME, "SET LEVEL:", level);
 	mod_timer(&my_timer, jiffies + msecs_to_jiffies(PERIOD));
 }
 
+/* proc_fc file operations */
 static struct proc_ops pops = {
 	.proc_read = mymod_read,
 };
 
+/* The read callback. It sends to userspace  current gpio level and duration of each level. */
 static ssize_t mymod_read(struct file *filp, char __user *buf, size_t count, loff_t *pos)
 {
-	sprintf(modbuf, "Current LED level: %d\nPeriod of the signal: %d\n", gpio_get_value(LED), PERIOD);
+	/*  Forming a string to send */
+	sprintf(modbuf, "Current LED level: %d\nPeriod of the signal (ms): %d\n", gpio_get_value(LED), PERIOD);
 
+	/* steps to adjust count properly and prevent reading beyond the file */
 	if (*pos >= BUFS)
 		return 0;
 	if (*pos + count > BUFS)
@@ -47,10 +61,13 @@ static ssize_t mymod_read(struct file *filp, char __user *buf, size_t count, lof
 	return count;
 }
 
+/* Init function */
 static int __init init_function(void)
 {
 
 	pr_info("%s: %s\n", DEVICE_NAME, "INIT");
+
+	/* Creation of a directory and a file in /proc */
 	proc_dir = proc_mkdir(PROC_NAME, NULL);
 	if (IS_ERR(proc_dir))
 		goto init_err;
@@ -60,32 +77,40 @@ static int __init init_function(void)
 		goto init_err;
 	}
 
+	/* Check whatever the GPIO is valid */
 	if (!gpio_is_valid(LED))
 		goto gpio_err;
 
 	gpio_free(LED);
-
+	/* If is freed, the next is not required */
+	/* Check if gpio is not busy. */
 	if (gpio_request_one(LED, GPIOF_INIT_LOW, "LED"))
 		goto gpio_err;
 
-        timer_setup(&my_timer, my_timer_callback, 0);
-	if (mod_timer(&my_timer, jiffies + msecs_to_jiffies(PERIOD))) {
-		pr_err("%s: %s\n", DEVICE_NAME, "Timer firing failed");
-		return -EPERM;
-	}
+	/* Timer setup */
+	timer_setup(&my_timer, my_timer_callback, 0);
+	if (mod_timer(&my_timer, jiffies + msecs_to_jiffies(PERIOD)))
+		goto timer_err;
 
 	return 0;
-	init_err:
-		proc_remove(proc_dir);
-		pr_err("%s: %s\n", DEVICE_NAME, "ENOMEM");
-		return -ENOMEM;
-	gpio_err:
-		proc_remove(proc_file);
-		proc_remove(proc_dir);
-		pr_err("%s: %s\n", DEVICE_NAME, "Invalid GPIO");
-		return -ENODEV;
-}
 
+init_err:
+	proc_remove(proc_dir);
+	pr_err("%s: %s\n", DEVICE_NAME, "Procfs Error");
+	return -ENOMEM;
+gpio_err:
+	proc_remove(proc_file);
+	proc_remove(proc_dir);
+	pr_err("%s: %s\n", DEVICE_NAME, "Invalid GPIO");
+	return -ENODEV;
+timer_err:
+	gpio_free(LED);
+	proc_remove(proc_file);
+	proc_remove(proc_dir);
+	pr_err("%s: %s\n", DEVICE_NAME, "Timer firing failed");
+	return -EPERM;
+}
+/* Exit function */
 static void __exit exit_function(void)
 {
 	gpio_free(LED);
@@ -101,4 +126,4 @@ module_exit(exit_function);
 
 MODULE_AUTHOR("Yurii Klysko");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Simle kernel module.");
+MODULE_DESCRIPTION("LED blinkong module with procfs file.");
