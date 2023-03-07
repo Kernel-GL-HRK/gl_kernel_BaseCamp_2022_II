e6c1783ddeb4fbe79366011ae3df5a998154b6a4
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-03-07 23:44:39 +0200

    Homework 2: Add copying of header files
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task2-bash/script1.sh
+++ task2-bash/script1.sh
@@ -2,7 +2,7 @@
 
 mkdir /tmp/guessanumber
 
-cp ../task1-simple-program/*.c /tmp/guessanumber
+cp ../task1-simple-program/*.{c, h} /tmp/guessanumber
 
 tar -zcvf guessanumber.tar.gz /tmp/guessanumber
 
