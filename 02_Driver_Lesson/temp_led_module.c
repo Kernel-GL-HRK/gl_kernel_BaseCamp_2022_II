12f55006caa80f4f2aa463b25f851a1b3bdc2b19
Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>
2023-03-07 18:45:31 +0100

    Driver_Lesson_02: Add code to temp_led_module.c file
    add two timers:
      timer_blink - Responsible for LED blinking;
      timer_thermal - responsible for the temperature measurement, the interval is 5 seconds. And switching the LEDs when the temperature rises.
    
    driver temp_led_module.ko test
    
    Signed-off-by: Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>

--- 02_Driver_Lesson/temp_led_module.c
+++ 02_Driver_Lesson/temp_led_module.c
@@ -1,3 +1,27 @@
+/*
+ *  @file        temp_led_module.ko
+ *  @author      Mykhailo Vyshnevskyi
+ *  @date        10 March 2023
+ *  @version     0.1.0
+ *  @details     A driver for measuring CPU temperature using the \"cpu-thermal\" 
+ *               sensor and indicating temperature using three LEDs. 
+ *  Work algorithm:
+ *               When starting the driver, all LEDs are on for 5 seconds.
+ *               The temperature measurement interval is 5 seconds.             
+ *               A GREEN LED blinks when the temperature is below 40 degrees, 
+ *               a YELLOW LED blinks when the temperature is below 60 degrees, and 
+ *               a RED LED blinks when the temperature is below 75 degrees. 
+ *               All LEDs remain on, and the RED LED blinking when the temperature is above 75 degrees. 
+ *               Additionally, the driver (hello/dummy) responds to the command 
+ *                        \"cat /proc/hello/dummy\" 
+ *               and outputs the message   
+ *                        \"temperature = %d.%d Grad\"
+ *  GPIO:
+ *               GPIO_5     RED 
+ *               GPIO_6     YELLOW
+ *               GPIO_26    GREEN
+ *               Tested with Linux raspberrypi [5.10.103+]
+ */
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -34,9 +58,66 @@ struct gpio led_array[] = {
     { GPIO_6, GPIOF_OUT_INIT_HIGH, "LED_6" },
     { GPIO_26, GPIOF_OUT_INIT_HIGH, "LED_26" }
 };
+static int gpio_index = 0;  // 0, 1, 2
+
+// Timer Variables
+#define TIMEOUT 5000   // milliseconds   (5s)
+static struct timer_list timer_blink, timer_thermal;
+static bool  flag_timer = false;
 
 static struct proc_dir_entry *proc_file;
 static struct proc_dir_entry *proc_folder;
+
+// Timer callback function for blinking LEDs
+void timer_blink_callback(struct timer_list * data)
+{    
+    if (flag_timer == false)
+    {
+        gpio_set_value(led_array[gpio_index].gpio, 0);
+        mod_timer(&timer_blink, jiffies + msecs_to_jiffies(10));
+        pr_info("led_array[gpio_index= %d].gpio = %d\n", gpio_index, led_array[gpio_index].gpio);
+    } else
+    {
+        gpio_set_value(led_array[gpio_index].gpio, 1);
+        mod_timer(&timer_blink, jiffies + msecs_to_jiffies(1));
+    }
+    flag_timer = !flag_timer;
+}
+
+// Callback function for thermal events
+void thermal_callback(struct timer_list * data)
+{
+    if (thermal_zone_get_temp(tz, &temp)) {
+        pr_err("thermal_callback >>>>> Failed to get temperature\n");
+    }
+    //pr_info("Temperature: %d\n", temp);
+    if (temp < 40000)                       // LED GREEN blinklicht
+    {   
+        gpio_index = 2;
+        gpio_set_value(GPIO_5, 0);
+        gpio_set_value(GPIO_6, 0);
+        gpio_set_value(GPIO_26, 1);
+    }else if (temp < 50000)                 // LED YELLOW blinklicht
+    {
+        gpio_index = 1;
+        gpio_set_value(GPIO_5, 0);
+        gpio_set_value(GPIO_6, 1);
+        gpio_set_value(GPIO_26, 0);
+    }else if (temp < 74000)                 // LED RED blinklicht
+    {
+        gpio_index = 0;
+        gpio_set_value(GPIO_5, 1);
+        gpio_set_value(GPIO_6, 0);
+        gpio_set_value(GPIO_26, 0);
+    }else                                   // LED RED blinklicht, YELLOW und GREEN licht
+    {
+        gpio_index = 0;                     
+        gpio_set_value(GPIO_5, 1);
+        gpio_set_value(GPIO_6, 1);
+        gpio_set_value(GPIO_26, 1);        
+    }
+    mod_timer(&timer_thermal, jiffies + msecs_to_jiffies(TIMEOUT));  
+}
 
 static ssize_t hello_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
@@ -102,11 +183,26 @@ static int __init hello_init(void)
         return -1;
     }
 
+    //Timer Init
+    timer_setup(&timer_blink, timer_blink_callback, 0); 
+    timer_setup(&timer_thermal, thermal_callback, 0); 
+
+    // Get Thermal Zone
+    tz = thermal_zone_get_zone_by_name("cpu-thermal");
+    if (!tz) {
+        pr_err("thermal_zone_get_zone_by_name error\n");
+        return -EINVAL;
+    }
+
     if (thermal_zone_get_temp(tz, &temp)) {
         pr_err("Failed to get temperature\n");
     } else {
         pr_info("CPU Temperature: %d\n", temp / 1000);
     }
+
+    // Start Timer
+    mod_timer(&timer_blink, jiffies + msecs_to_jiffies(TIMEOUT));
+    mod_timer(&timer_thermal, jiffies + msecs_to_jiffies(TIMEOUT));
 
     pr_info("Hello Module Inserted /proc/%s/%s\n", PROC_DIR_NAME, PROC_FILE_NAME);
 
@@ -115,6 +211,9 @@ static int __init hello_init(void)
 
 static void __exit hello_exit(void)
 {
+    // Remove Timer
+    del_timer(&timer_blink);
+    del_timer(&timer_thermal);
     // LED Driver De-registration
     gpio_free_array(led_array, ARRAY_SIZE(led_array));
     // Remove Proc File
@@ -127,6 +226,6 @@ module_init(hello_init);
 module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("V. M.");
-MODULE_DESCRIPTION("A");
+MODULE_AUTHOR("Vyshnevskiy Mykhailo");
+MODULE_DESCRIPTION("A driver for measuring CPU temperature using the \"cpu-thermal\" sensor and indicating temperature using three LEDs: a green LED blinks when the temperature is below 40 degrees, a yellow LED blinks when the temperature is below 60 degrees, and a red LED blinks when the temperature is below 75 degrees. All LEDs remain on when the temperature is above 75 degrees, with the red LED blinking. Additionally, the driver responds to the command \"cat /proc/hello/dummy\" and outputs the message \"temperature = %d.%d Grad\".");
 
