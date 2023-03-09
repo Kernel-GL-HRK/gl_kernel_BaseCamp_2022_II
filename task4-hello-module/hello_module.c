19af4dc8e38be692d1dcd3f6d4309aab4c1e8b76
Viacheslav Us <slava.us@icloud.com>
2023-03-09 22:42:07 +0200

    TASK04: Implement simple hello kernel module
    Signed-off-by: Viacheslav Us <slava.us@icloud.com>

---
+++ task4-hello-module/hello_module.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Viacheslav Us");
+MODULE_DESCRIPTION("First hello kernel module");
+MODULE_VERSION("0.1");
+
+static int __init hello_init(void)
+{
+	pr_info("Hello World!\n");
+	
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Goodbye World!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
