b1063319431fe75a8530d72852a473829580e3e3
Kushnir-Vladyslav <vlad987321@gmail.com>
2023-03-10 20:49:49 +0200

    Task5: implement procfs
    Signed-off-by: Kushnir-Vladyslav <vlad987321@gmail.com>

--- task5-procfs-gpio-timer/module.c
+++ task5-procfs-gpio-timer/module.c
@@ -4,6 +4,8 @@
 #include <linux/init.h>
 #include <linux/gpio.h>
 #include <linux/err.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 #include <linux/timer.h>
 #include <linux/jiffies.h>
 
@@ -14,10 +16,44 @@ MODULE_VERSION("0.1");
 
 #define GPIO_21 (21)
 
+#define PROC_BUFFER_SIZE 100
+
+static char proc_buffer[PROC_BUFFER_SIZE];
+
+static struct proc_dir_entry *proc_file;
+static struct proc_dir_entry *proc_folder;
+
+#define PROC_FILE_NAME "my_proc_mod_file"
+#define PROC_DIR_NAME "my_proc_mod_dir"
+
 #define TIMEOUT (1000 / 20)
 static struct timer_list etx_timer;
 static unsigned int count;
 static bool status;
+
+static bool is_read;
+
+static ssize_t proc_read(struct file *FIle, char __user *buffer, size_t Count, loff_t *offset)
+{
+	is_read = !is_read;
+	if (!is_read)
+		return 0;
+
+	size_t size_to_copy  = sprintf(proc_buffer, "LED status: %d.\nPeriod time: %d ms.\nNumber of switches: %d.\n", status, TIMEOUT, count);
+	size_t not_copy = copy_to_user(buffer, proc_buffer, size_to_copy);
+
+	return size_to_copy - not_copy;
+}
+
+static ssize_t proc_write(struct file *FIle, const char __user *buffer, size_t count, loff_t *offset)
+{
+	return count;
+}
+
+static struct proc_ops fops = {
+	.proc_read = proc_read,
+	.proc_write = proc_write
+};
 
 static void timer_callback(struct timer_list *data)
 {
@@ -45,6 +81,17 @@ static int __init init(void)
 
 	gpio_direction_output(GPIO_21, 0);
 
+	proc_folder = proc_mkdir(PROC_DIR_NAME, NULL);
+	if (!proc_folder) {
+		pr_err("ERROR: Could not create /proc/%s/ folder\n", PROC_DIR_NAME);
+		goto gpio_exit;
+	}
+
+	proc_file = proc_create(PROC_FILE_NAME, 0666, proc_folder, &fops);
+	if (!proc_file) {
+		pr_err("ERROR: Could not initialize /proc/%s/%s\n", PROC_DIR_NAME, PROC_FILE_NAME);
+		goto proc_exit;
+	}
 
 	timer_setup(&etx_timer, timer_callback, 0);
 
@@ -52,6 +99,10 @@ static int __init init(void)
 
 	pr_info("The initalization of module 'Procfs, GPIO, timer...' is successeful.");
 	return 0;
+
+proc_exit:
+	proc_remove(proc_file);
+	proc_remove(proc_folder);
 
 gpio_exit:
 	gpio_free(GPIO_21);
@@ -63,6 +114,9 @@ exit:
 static void __exit exit_module(void)
 {
 	del_timer(&etx_timer);
+
+	proc_remove(proc_file);
+	proc_remove(proc_folder);
 
 	gpio_set_value(GPIO_21, 0);
 
