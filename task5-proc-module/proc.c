58721b7f59497a7e0c20d6f0513694f32b838ea1
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-03-11 17:41:51 +0200

    Homework 5: Add gpio_status file operations
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task5-proc-module/proc.c
+++ task5-proc-module/proc.c
@@ -1,4 +1,6 @@
 #include <linux/kernel.h>
+#include <linux/uaccess.h>
+#include <linux/module.h>
 #include <linux/proc_fs.h>
 #include "proc.h"
 #include "gpio.h"
@@ -9,6 +11,47 @@ static struct proc_dir_entry *proc_folder;
 
 static const size_t PROC_BUFFER_CAPACITY = 8;
 static char procfs_buffer[PROC_BUFFER_CAPACITY];
+
+static ssize_t gpio_status_read(struct file *file, char __user *buffer, size_t count, loff_t *offset)
+{
+	if (*offset > 0)
+		return 0;
+
+	ssize_t to_copy, not_copied, buffer_size;
+
+	buffer_size = sprintf(procfs_buffer, "%d\n", gpio_get_status());
+
+	if (buffer_size < 0)
+		return -EFAULT
+
+	to_copy = min_t(size_t, count, buffer_size);
+	not_copied = copy_to_user(buffer, procfs_buffer, to_copy);
+
+	*offset += to_copy - not_copied;
+
+	return to_copy - not_copied;
+}
+
+static ssize_t gpio_status_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
+{
+	ssize_t to_copy, not_copied;
+
+	to_copy = 1;
+	not_copied = copy_from_user(procfs_buffer, buffer, to_copy);
+
+	switch (procfs_buffer[0]) {
+		case '1': gpio_blink_on(); break;
+		case '0': gpio_blink_off(); break;
+	}
+
+	return to_copy - not_copied;
+}
+
+static struct proc_ops gpio_status_ops = {
+	.proc_read	= gpio_status_read,
+	.proc_write	= gpio_status_write
+};
+
 
 static ssize_t timeout_read(struct file *file, char __user *buffer, size_t count, loff_t *offset)
 {
