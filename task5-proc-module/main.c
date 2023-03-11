9d07760b97ee64b2ea5c38eef0b753586b6b9ee7
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-03-11 17:46:21 +0200

    Homework 5: Update module init
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task5-proc-module/main.c
+++ task5-proc-module/main.c
@@ -14,12 +14,26 @@ MODULE_VERSION("1.0");
 static int driver_init(void)
 {
 	pr_info("module init");
+	
+	if (gpio_init(27)) {
+		pr_err("Failed to init gpio\n");
+		return -EFAULT;
+	}
+
+	if (proc_init()) {
+		pr_err("Failed to init proc\n");
+		gpio_exit();
+		return -EFAULT;
+	}
+
 	return 0;
 }
 /*===============================================================================================*/
 static void driver_exit(void)
 {
 	pr_info("module exit");
+	gpio_exit();
+	proc_exit();
 }
 /*===============================================================================================*/
 
