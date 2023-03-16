eb1ddae60ad33bc83cfe37c8ed6eda51cd1d95bb
Sergiy Us <sergiy.us@gmail.com>
2023-03-16 17:00:32 +0200

    TASK05: Add timer function
    The timer interval is based on the TIMEOUT macro and is 1000ms.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

--- task5-procfs-led/module.c
+++ task5-procfs-led/module.c
@@ -4,6 +4,8 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
 #include <linux/version.h>
 
 MODULE_LICENSE("GPL");
@@ -18,17 +20,23 @@ MODULE_VERSION("0.1");
 #define PROC_BUFFER_SIZE	100
 #define PROC_DIR_NAME		"hello"		/* /proc/hello/dummy */
 #define PROC_FILE_NAME		"dummy"
+#define TIMEOUT			1000		/* milliseconds */
 
 static char   procfs_buffer[PROC_BUFFER_SIZE] = {0};
 static size_t procfs_buffer_size;
+static unsigned int timer_count;
+
 static struct proc_dir_entry *proc_file;
 static struct proc_dir_entry *proc_dir;
+static struct timer_list etx_timer;
 
 static ssize_t hello_read(struct file *File, char __user *buffer,
 			  size_t count, loff_t *offset);
 static ssize_t hello_write(struct file *file, const char __user *buffer,
 			   size_t count, loff_t *offset);
 static inline int proc_init(void);
+static void timer_callback(struct timer_list *data);
+static inline void timer_init(void);
 
 #ifdef HAVE_PROC_OPS
 static const struct proc_ops fops = {
@@ -47,12 +55,16 @@ static int __init hello_init(void)
 	int ret;
 
 	ret = proc_init();
+	timer_init();
 
 	return ret;
 }
 
 static void __exit hello_exit(void)
 {
+	del_timer(&etx_timer);
+	pr_info("HELLO: TIMER: %s()\n", __func__);
+
 	proc_remove(proc_file);
 	proc_remove(proc_dir);
 	pr_info("HELLO: PROC: /proc/%s/%s removed\n",
@@ -76,9 +88,9 @@ static ssize_t hello_read(struct file *File, char __user *buffer,
 
 	delta = to_copy - not_copied;
 
-	pr_debug("HELLO: PROC: DEBUG: count(read) = %d\n", count);
-	pr_debug("HELLO: PROC: DEBUG: delta(read) = %d\n", delta);
-	pr_debug("HELLO: PROC: DEBUG: procfs_buffer_size(read) = %d\n",
+	pr_debug("DEBUG: PROC: count(read) = %d\n", count);
+	pr_debug("DEBUG: PROC: delta(read) = %d\n", delta);
+	pr_debug("DEBUG: PROC: procfs_buffer_size(read) = %d\n",
 		 procfs_buffer_size);
 
 	procfs_buffer_size -= delta;
@@ -97,7 +109,9 @@ static ssize_t hello_write(struct file *file, const char __user *buffer,
 	delta = to_copy - not_copied;
 
 	procfs_buffer_size = delta;
-	pr_debug("HELLO: PROC: DEBUG: procfs_buffer_size(write) = %d\n",
+
+	pr_debug("DEBUG: PROC: count(write) = %d\n", count);
+	pr_debug("DEBUG: PROC: procfs_buffer_size(write) = %d\n",
 		 procfs_buffer_size);
 
 	return delta;
@@ -132,3 +146,19 @@ exit:
 	return ret;
 }
 
+static void timer_callback(struct timer_list *data)
+{
+	pr_debug("DEBUG: TIMER: %s() %d times\n", __func__, timer_count++);
+	mod_timer(&etx_timer, jiffies + msecs_to_jiffies(TIMEOUT));
+}
+
+static inline void timer_init(void)
+{
+	/* setup your timer to call my_timer_callback */
+	timer_setup(&etx_timer, timer_callback, 0);
+
+	/* setup timer interval is based on TIMEOUT Macro */
+	mod_timer(&etx_timer, jiffies + msecs_to_jiffies(TIMEOUT));
+
+	pr_info("HELLO: TIMER: %s()\n", __func__);
+}
