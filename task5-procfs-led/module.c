586269d339b2f7ef01598e98d20f72a0150dc7b5
Sergiy Us <sergiy.us@gmail.com>
2023-03-16 16:52:39 +0200

    TASK05: Add procfs (read/write)
    1. module.c is a simple Linux kernel driver that allows reading/writing
       to /proc/hello/dummy.
       The size of the buffer is 100 bytes.
       File checked by checkpatch.pl.
    2. Added -DDEBUG key to Makefile - to print debug info in dmesg.
    3. module.ko - compiled for Raspberry Pi Zero HW (Linux 5.15.84+,
       arm6l).
    4. Added README file with task and .gitignore.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

---
+++ task5-procfs-led/module.c
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/version.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sergiy Us");
+MODULE_DESCRIPTION("A simple device driver - procfs module");
+MODULE_VERSION("0.1");
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+#define	HAVE_PROC_OPS
+#endif
+
+#define PROC_BUFFER_SIZE	100
+#define PROC_DIR_NAME		"hello"		/* /proc/hello/dummy */
+#define PROC_FILE_NAME		"dummy"
+
+static char   procfs_buffer[PROC_BUFFER_SIZE] = {0};
+static size_t procfs_buffer_size;
+static struct proc_dir_entry *proc_file;
+static struct proc_dir_entry *proc_dir;
+
+static ssize_t hello_read(struct file *File, char __user *buffer,
+			  size_t count, loff_t *offset)
+{
+	ssize_t to_copy, not_copied, delta;
+
+	if (!procfs_buffer_size)
+		return 0;
+
+	to_copy = min(count, procfs_buffer_size);
+
+	not_copied = copy_to_user(buffer, procfs_buffer, to_copy);
+
+	delta = to_copy - not_copied;
+
+	pr_debug("HELLO: PROC: DEBUG: count(read) = %d\n", count);
+	pr_debug("HELLO: PROC: DEBUG: delta(read) = %d\n", delta);
+	pr_debug("HELLO: PROC: DEBUG: procfs_buffer_size(read) = %d\n",
+		 procfs_buffer_size);
+
+	procfs_buffer_size -= delta;
+
+	return delta;
+}
+
+static ssize_t hello_write(struct file *file, const char __user *buffer,
+			   size_t count, loff_t *offset)
+{
+	ssize_t to_copy, not_copied, delta;
+
+	to_copy = min(count, sizeof(procfs_buffer));
+
+	not_copied = copy_from_user(procfs_buffer, buffer, to_copy);
+	delta = to_copy - not_copied;
+
+	procfs_buffer_size = delta;
+	pr_debug("HELLO: PROC: DEBUG: procfs_buffer_size(write) = %d\n",
+		 procfs_buffer_size);
+
+	return delta;
+}
+
+#ifdef HAVE_PROC_OPS
+static const struct proc_ops fops = {
+	.proc_read  = hello_read,
+	.proc_write = hello_write,
+};
+#else
+static const struct file_operations fops = {
+	.read  = hello_read,
+	.write = hello_write,
+};
+#endif
+
+static int __init hello_init(void)
+{
+	proc_dir = proc_mkdir(PROC_DIR_NAME, NULL);
+	if (!proc_dir) {
+		pr_err("HELLO: ERROR: Could not create /proc/%s directory\n",
+		       PROC_DIR_NAME);
+		return -ENOMEM;
+	}
+
+	proc_file = proc_create(PROC_FILE_NAME, 0666, proc_dir, &fops);
+	if (!proc_file) {
+		pr_err("HELLO: ERROR: Could not initialize /proc/%s/%s\n",
+		       PROC_DIR_NAME, PROC_FILE_NAME);
+		proc_remove(proc_file);
+		proc_remove(proc_dir);
+		return -ENOMEM;
+	}
+
+	pr_info("HELLO: PROC: /proc/%s/%s created\n",
+		PROC_DIR_NAME, PROC_FILE_NAME);
+
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	proc_remove(proc_file);
+	proc_remove(proc_dir);
+	pr_info("HELLO: PROC: /proc/%s/%s removed\n",
+		PROC_DIR_NAME, PROC_FILE_NAME);
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
