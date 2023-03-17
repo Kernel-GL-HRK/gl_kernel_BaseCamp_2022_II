1039fd1e831c474b4e870fcbf74ac17836bee4b6
Yurii.Bezkorovainyi <13736343+bezkor@users.noreply.github.com>
2023-03-17 16:36:10 +0200

    TASK01: GPIO_BLINK_TIMER_PROC: Add timer
    Add timer & callback prototype to src

--- TASK01_GPIO_BLINK_TIMER_PROC/gbtp.c
+++ TASK01_GPIO_BLINK_TIMER_PROC/gbtp.c
@@ -2,20 +2,47 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/moduleparam.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Yurii Bezkor");
-MODULE_DESCRIPTION("Hello world kernel module");
-MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("ModuleParam feature");
+MODULE_VERSION("0.2");
+
+static int led_pin = -1;
+module_param(led_pin, int, 0644);
+MODULE_PARM_DESC(led_pin, "Led pin number for Blink");
+
+static unsigned int timer_count = 1000;
+module_param(timer_count, int, 0644);
+MODULE_PARM_DESC(timer_count, "Timer period");
+
+static struct timer_list timer_gbtp;
+
+void timer_callback(struct timer_list *data)
+{
+	pr_info("GBTP: Timer call\n");
+	mod_timer(&timer_gbtp, jiffies + msecs_to_jiffies(timer_count));
+}
 
 static int __init gbtp_init(void)
 {
-	pr_info("GBTP: Hello Kernel!\n");
+	pr_info("GBTP: LedPin = %d\n", led_pin);
+	pr_info("GBTP: TimerC = %d\n", timer_count);
+
+	timer_setup(&timer_gbtp, timer_callback, 0);
+	mod_timer(&timer_gbtp, jiffies + msecs_to_jiffies(timer_count));
+
+	pr_info("GBTP: Load success!\n");
 	return 0;
 }
 static void __exit gbtp_exit(void)
 {
-	pr_info("GBTP: Goodbye Kernel!\n");
+	del_timer(&timer_gbtp);
+	pr_info("GBTP: Unload done!\n");
 }
 
 module_init(gbtp_init);
