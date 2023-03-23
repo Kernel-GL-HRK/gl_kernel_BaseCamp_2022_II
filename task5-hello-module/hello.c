71ab1a296b3a5ab1ab2e0c7daaed9f6790d9fc95
Kriz <kruzhanivska.olena212@gmail.com>
2023-03-23 22:23:44 +0200

    TASK5: Code formatting

--- task5-hello-module/hello.c
+++ task5-hello-module/hello.c
@@ -6,7 +6,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kriz");
-MODULE_DESCRIPTION("Hello_world module with simple canculator (sum, sub and mul)");
+MODULE_DESCRIPTION("Hello_world module with simple calculator (sum, sub and mul)");
 MODULE_VERSION("0.1");
 
 static int op1 = -1;
@@ -17,16 +17,18 @@ static int op2 = -1;
 module_param(op2, int, 00644);
 MODULE_PARM_DESC(op2, "Second operator for calculation");
 
-static int __init hello_init(void){
-pr_info("HELLO: Hello, world!\nCalculations:\n");
-pr_info("HELLO:Sum: %d + %d = %d\n", op1, op2, op1 + op2);
-pr_info("HELLO:Sub: %d - %d = %d\n", op1, op2, op1 - op2);
-pr_info("HELLO:Mul: %d * %d = %d\n", op1, op2, op1 * op2);
-return 0;
+static int __init hello_init(void)
+{
+	pr_info("HELLO: Hello, world!\nCalculations:\n");
+	pr_info("HELLO:Sum: %d + %d = %d\n", op1, op2, op1 + op2);
+	pr_info("HELLO:Sub: %d - %d = %d\n", op1, op2, op1 - op2);
+	pr_info("HELLO:Mul: %d * %d = %d\n", op1, op2, op1 * op2);
+	return 0;
 }
 
-static void __exit hello_exit(void){
-pr_info("HELLO: Goodbye, world!\n");
+static void __exit hello_exit(void)
+{
+	pr_info("HELLO: Goodbye, world!\n");
 }
 
 module_init(hello_init);
