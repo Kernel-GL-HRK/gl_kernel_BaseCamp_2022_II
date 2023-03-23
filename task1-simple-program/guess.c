1dc93466c9884de1133f26246f705efa9646efb4
Kriz <kruzhanivska.olena212@gmail.com>
2023-03-23 20:51:00 +0200

    Task1.5: Edit code after checkpatch

--- task1-simple-program/guess.c
+++ task1-simple-program/guess.c
@@ -1,30 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
-int compNumber = 0;
-
-void generateRandom()
+void generateRandom(void)
 {
 	srand(time(0));
-	compNumber = rand()%10;
+	int compNumber = rand()%10;
 }
 
-int main()
+int main(void)
 {
-    int guestNumber;
-    printf("Welcome to Guess a number!\nEnter your number (0-9):");
-    scanf("%d", &guestNumber);
-    generateRandom();
+	int guestNumber;
 
-    if(compNumber==guestNumber)
-    {
-        printf("You win!\n");
-        return 0;
-    }
-    else
-    {
-        printf("You loose!\n");
-        return 1;
-    }
+	printf("Welcome to Guess a number!\nEnter your number (0-9):");
+	scanf("%d", &guestNumber);
+	generateRandom();
+
+	if (compNumber == guestNumber) {
+		printf("You win!\n");
+	} else {
+		printf("You loose!\n");
+		return 1;
+	}
+	return 0;
 }
