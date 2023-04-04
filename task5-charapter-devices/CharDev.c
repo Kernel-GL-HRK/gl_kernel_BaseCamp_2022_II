f814f0ec7de531797612d17a543d6d0f4cce1d3b
Roman Muravka <roman.muravka@nure.ua>
2023-04-04 13:26:27 +0300

    Task_5: Change - CharDev.c(dev_ops, proc_ops), CharDev.ko
    Modified CharDev.c file by adding device options (read(), write(), close(), open()) and proc_ops(read()) to dislay information about device and its status.
    Builded Chardev.ko file from CharDev.c file for downloading as a module of kernel;
    
    Signed-off-by: Roman Muravka <roman.muravka@nure.ua>

--- task5-charapter-devices/CharDev.c
+++ task5-charapter-devices/CharDev.c
@@ -1,3 +1,4 @@
+#include <linux/string.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/err.h>
@@ -14,29 +15,52 @@
 MODULE_AUTHOR("Muravka Roman");
 MODULE_DESCRIPTION("Simple charapter device - Matrix keypad");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.1");
+MODULE_VERSION("1.2");
 
 #define MAX_BUFFER 1024
+#define MAX_CLEAN_BUFFER 32
+//Dmesg defines
+#define PR_START_CALC		0
+#define PR_LOC_SEPARATOR	1
+#define PR_GL_SEPARATOR		2
+#define PR_CLEAR_OPERATOR	3
+#define PR_CLEAR_NUM		4
+#define PR_CLEAR_EMPTY		5
+#define PR_ERR_ZERO			6
+#define PR_ERR_OPERATOR 	7
+#define PR_OPERANDS			8
+#define PR_RESULT			9
+
 // Proc defines
 #define PROC_DIR_NAME "Keypad_Matrix"
 #define PROC_FILE_NAME "Driver_Read"
-#define PROC_BUFFER = "Matrix keys:\n"\
-						"|-----------------|\n"\
-						"| [1] [2] [3] [A] |\n"\
-						"| [4] [5] [6] [B] |\n"\
-						"| [7] [8] [9] [C] |\n"\
-						"| [*] [0] [#] [D] |\n"\
-						"|-----------------|\n"\
-						"Number keys(0-9) responsible for numerics.\n"\
-						"\"A\" - (+) add;\n"\
-						"\"B\" - (-) subtract;\n"\
-						"\"C\" - (*) multiply;\n"\
-						"\"D\" - (/) divide;\n"\
-						"\"*\" - (<-) clean a symbol"\
-						"\"#\" - (=) equal and finish calculation"
+#define PROC_BUFFER "Matrix keys:\n"\
+					"|-----------------|\n"\
+					"| [1] [2] [3] [A] |\n"\
+					"| [4] [5] [6] [B] |\n"\
+					"| [7] [8] [9] [C] |\n"\
+					"| [*] [0] [#] [D] |\n"\
+					"|-----------------|\n"\
+					"Number keys(0-9) responsible for numerics.\n"\
+					"\"A\" - (+) add;\n"\
+					"\"B\" - (-) subtract;\n"\
+					"\"C\" - (*) multiply;\n"\
+					"\"D\" - (/) divide;\n"\
+					"\"*\" - (<-) clean a symbol\n"\
+					"\"#\" - (=) equal and finish calculation\n"
 // Defines of charatper device
 #define CLASS_NAME "Keypad"
 #define DEVICE_NAME "Matrix"
+#define FIRST_NOTICE 	"----------------------------------------------------------------------\n"\
+						"Calculator status messages will be displayed in - ?\n"\
+						"To change calculator output direction, write in /dev/Matrix file:\n"\
+						"\t1 - dmesg (output in syslog file);\n"\
+						"\t2 - /dev/Matrix (output in device file);\n"\
+						"\t3 - dmesg and /dev/Matrix.\n"\
+						"----------------------------------------------------------------------\n"
+#define C1 "dmesg"
+#define C2 "/dev/Matrix"
+#define C3 "dmesg and /dev/Matrix"
 // GPIO defines
 #define ON 1
 #define OFF 0
@@ -53,7 +77,13 @@ MODULE_VERSION("1.1");
 static struct class *class_tree;
 static struct device *device_file;
 static struct cdev device;
+uint8_t buffer_chrdev[MAX_BUFFER] = {0};
 dev_t MM = 0;
+enum calc_out_e{
+	DMESG = 1,
+	DEV = 2,
+	ALL = 3
+}calc_out = DEV;
 // Matrix variables
 const uint8_t rows[] = {ROW1, ROW2, ROW3, ROW4};
 const uint8_t cols[] = {COL1, COL2, COL3, COL4};
@@ -83,8 +113,8 @@ struct calc_s {
 	int32_t number_02;
 	int32_t result;
 	uint8_t operator;
-}calc = {0, 0, 0, 0};
-
+}calc = {0, 0, 0xFFFFFFFF, 0};
+uint8_t clearBuffer[MAX_CLEAN_BUFFER], last = 0;
 int debounce(int gpio)
 {
 	int i = 0;
@@ -97,11 +127,51 @@ int debounce(int gpio)
 	return 0;
 }
 
+void pr_calc(uint32_t pr_param){
+	if((calc_out == DMESG) || (calc_out == ALL))
+		switch(pr_param){
+			case PR_START_CALC:
+				pr_notice("|matrix-calculator: CALCULATOR IS STARTED\n");
+				break;
+			case PR_LOC_SEPARATOR:
+				pr_info("------------------------------------------------------------\n");
+				break;
+			case PR_GL_SEPARATOR:
+				pr_info("------------------------matrix------------------------------\n");
+				break;
+			case PR_CLEAR_OPERATOR:
+				pr_info("|matrix-clear: operator %c is delete\n", clearBuffer[last]);
+				break;
+			case PR_CLEAR_NUM:
+				pr_info("|matrix-calculator-clear: numeric %c is delete\n", clearBuffer[last]);
+				break;
+			case PR_CLEAR_EMPTY:
+				pr_info("|matrix-calculator-clear: buffer is empty\n");
+				break;
+			case PR_ERR_OPERATOR:
+				pr_err("|matrix-calculator-error: operator(+,-,*,/) did not enter!\n");
+				break;
+			case PR_ERR_ZERO:
+				pr_err("|matrix-calculator-error: can not divide by zero\n");
+				break;
+			case PR_OPERANDS:
+				pr_info("|matrix-calculator: I{%d} %c II{%d}\n", calc.number_01, (calc.operator == '\0') ? '?' : calc.operator, calc.number_02);
+				break;
+			case PR_RESULT:
+					pr_info("|matrix-calculator: %d %c %d = %d\n", calc.number_01, calc.operator, calc.number_02, calc.result);
+				break;
+	}
+}
+
 int matrix_check_key(void) {
 	uint8_t row_num = 0, col_num = 0;
-	uint8_t last = 0, clearBuffer[32] = {0};
 	uint32_t temp = 0;
+
 	clearBuffer[last] = 0xFF;
+	for(last = 1; clearBuffer < MAX_CLEAN_BUFFER; last++)
+		clearBuffer[last] = 0;
+	last = 0;
+
 	while (1) {
 		gpio_direction_output(rows[row_num], ON);
 
@@ -123,21 +193,21 @@ int matrix_check_key(void) {
 						}
 						break;
 					case '<':
-						pr_info("|----------------------------------------------------------|\n");
+						pr_calc(PR_LOC_SEPARATOR);
 						if ((clearBuffer[last] >= '0') && (clearBuffer[last] <= '9')){
 							temp /= 10;
-							pr_info("|matrix-calculator-clear: numeric %c is delete              |\n", clearBuffer[last]);
+							pr_calc(PR_CLEAR_NUM);
 							last--;
 						}
 						else if(clearBuffer[last] == 0xFF)
-							pr_info("matrix-calculator-clear: buffer is empty                   |\n");
+							pr_calc(PR_CLEAR_EMPTY);
 						else {
 							calc.operator = 0;
 							temp = calc.number_01;
-							pr_info("|matrix-clear: operator %c is delete                        |\n", clearBuffer[last]);
+							pr_calc(PR_CLEAR_OPERATOR);
 							last--;
 						}
-						pr_info("|----------------------------------------------------------|\n");
+						pr_calc(PR_LOC_SEPARATOR);
 						break;
 					case '=':
 							switch(calc.operator){
@@ -152,22 +222,19 @@ int matrix_check_key(void) {
 									break;
 								case '/':
 									if(calc.number_02 == 0){
-										pr_err("|matrix-calculator-error: can not divide by zero           |\n");
-										pr_info("|-----------------------matrix-----------------------------|\n");
+										pr_calc(PR_ERR_ZERO);
 										return 0;
 									}
 									else
 										calc.result = calc.number_01 / calc.number_02;
 									break;
 								default: 
-									pr_err("|matrix-error: operator(+,-,*,/) did not enter!            |\n");
-									pr_info("|-----------------------matrix-----------------------------|\n");
+									pr_calc(PR_ERR_OPERATOR);
 									return 0;
 									break;
 							}
 									
-							pr_info("|matrix-calculator: %2d %c %2d = %4d                       |\n", calc.number_01, calc.operator, calc.number_02, calc.result);
-							pr_info("|-----------------------matrix-----------------------------|\n");
+							pr_calc(PR_RESULT);
 							return 0;
 						}
 					if(calc.operator == '\0')
@@ -175,7 +242,7 @@ int matrix_check_key(void) {
 					else
 						calc.number_02 = temp;
 						
-					pr_info("|matrix-calculator: I{%2d} %c II{%2d}                         |\n", calc.number_01, (calc.operator == '\0') ? '?' : calc.operator, calc.number_02);
+					pr_calc(PR_OPERANDS);
 					if(key[row_num][col_num] != '<')
 						if((key[row_num][col_num] >= '0') && (key[row_num][col_num] <= '9'))
 							clearBuffer[++last] = key[row_num][col_num];
@@ -188,11 +255,123 @@ int matrix_check_key(void) {
 	}
 }
 
+static void output_choice_message(void)
+{
+	uint16_t i = 0, j = 0, alt = 0;
+	while(FIRST_NOTICE[j] != '\0'){
+		if(FIRST_NOTICE[j] == '?'){
+			switch(calc_out){
+				case DMESG:
+					for(alt = 0; C1[alt] != '\0'; alt++)
+						buffer_chrdev[i++] = C1[alt];
+					break;
+				case DEV:
+					for(alt = 0; C2[alt] != '\0'; alt++)
+						buffer_chrdev[i++] = C2[alt];
+					break;
+				case ALL:
+					for(alt = 0; C3[alt] != '\0'; alt++)
+						buffer_chrdev[i++] = C3[alt];
+					break;
+			}
+			j++;
+		}
+		else
+			buffer_chrdev[i++] = FIRST_NOTICE[j++];
+	}
+	while(i < sizeof(buffer_chrdev))
+		buffer_chrdev[i++] = '\0';
+}
+
 // Charapter file operations
-static int dev_open(struct inode *inodep, struct file *filep) { return 0; }
-static int dev_close(struct inode *inodep, struct file *filep) { return 0; }
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) { return 0; }
-static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) { return 0; }
+static int dev_open(struct inode *inodep, struct file *filep)
+{ 
+	pr_info("matrix-chrdev: file /dev/%s is opened\n", DEVICE_NAME);
+	return 0; 
+}
+static int dev_close(struct inode *inodep, struct file *filep)
+{
+	pr_info("matrix-chrdev: file /dev/%s is closed\n", DEVICE_NAME);
+	return 0;
+}
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
+{
+	ssize_t to_copy = sizeof(buffer_chrdev);
+	uint16_t end = 0;
+
+	if((*offset == 0)){
+		output_choice_message();
+		*offset = *offset + 1;
+	}
+	else if(calc_out == DEV || calc_out == ALL){
+		if((calc.operator == '\0') && (calc.result == 0xFFFFFFFF))
+			to_copy = snprintf(buffer_chrdev, sizeof(buffer_chrdev), "\rI{%d} ? II{%d} = ??          ", calc.number_01, calc.number_02);
+		else if(calc.result == 0xFFFFFFFF){
+		 	switch(calc.operator){
+				case '+':
+					to_copy = snprintf(buffer_chrdev, sizeof(buffer_chrdev), "\rI{%d} %c II{%d} = %-10d", calc.number_01, calc.operator, calc.number_02, calc.number_01 + calc.number_02);
+					break;
+				case '-':
+					to_copy = snprintf(buffer_chrdev, sizeof(buffer_chrdev), "\rI{%d} %c II{%d} = %-10d", calc.number_01, calc.operator, calc.number_02, calc.number_01 - calc.number_02);
+					break;
+				case '*':
+					to_copy = snprintf(buffer_chrdev, sizeof(buffer_chrdev), "\rI{%d} %c II{%d} = %-10d", calc.number_01, calc.operator, calc.number_02, calc.number_01 * calc.number_02);
+					break;
+				case '/':
+					if(calc.number_02 == 0)
+						to_copy = snprintf(buffer_chrdev, sizeof(buffer_chrdev), "\rI{%d} %c II{%d} = infinity", calc.number_01, calc.operator, calc.number_02);
+					else
+						to_copy = snprintf(buffer_chrdev, sizeof(buffer_chrdev), "\rI{%d} %c II{%d} = %-10d", calc.number_01, calc.operator, calc.number_02, calc.number_01 / calc.number_02);
+					break;
+			}
+		}
+		else{
+			to_copy = snprintf(buffer_chrdev, sizeof(buffer_chrdev), "\n!!!Calculating is finished!!!\n");
+			*offset = *offset + 1;
+		}
+	}
+
+	if(*offset < 3){
+			to_copy = min(to_copy, len);
+			if(copy_to_user(buffer, buffer_chrdev, to_copy) != 0){
+				pr_err("matrix-chrdev: dont have space to output data into file");
+				return -ENOSPC;
+			}
+			if(*offset == 2)
+				*offset = *offset + 1;
+			return to_copy;
+	}
+	return 0;
+}
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
+{
+	uint8_t wr_buffer[MAX_BUFFER] = {0};
+	uint32_t i;
+	if(copy_from_user(wr_buffer, buffer, len)){
+		pr_err("matrix-chrdev-error: can not read data from user");
+		return -ENOSPC;
+	}
+	pr_info("matrix-chrdev: data is received");
+	for(i = 0; i < MAX_BUFFER; i++){
+		switch(wr_buffer[i]){
+			case '1':
+				calc_out = DMESG;
+				pr_info("matrix-chrdev: calculator output - %s", C1);
+				return len;
+			case '2':
+				calc_out = DEV;
+				pr_info("matrix-chrdev: calculator output - %s", C2);
+				return len;
+			case '3':
+				calc_out = ALL;
+				pr_info("matrix-chrdev: calculator output - %s", C3);
+				return len;
+		}
+	}
+	pr_err("matrix-chrdev: entered wrong choice number, calculator output - %s", C3);
+	calc_out = ALL;
+	return len;
+}
 
 static struct file_operations dev_fops = {
 		.open = dev_open,
@@ -205,17 +384,25 @@ static struct file_operations dev_fops = {
 
 static struct proc_dir_entry *proc_file;
 static struct proc_dir_entry *proc_dir;
-
 // Proc file operations
-static ssize_t proc_read(struct file *filep, char __user *buffer, size_t count, loff_t *offset)
+static ssize_t procFS_read(struct file *filep, char __user *buffer, size_t count, loff_t *offset)
 {
-	ssize_t to_copy, not_copied, copied;
-	
-	return 0; 
+    ssize_t to_copy = 0;
+    to_copy = min(count, sizeof(PROC_BUFFER));
+	if(*offset != sizeof(PROC_BUFFER)){
+    	if(copy_to_user(buffer, PROC_BUFFER, to_copy) != 0)
+			return -ENOSPC;
+		else{ 
+			*offset = to_copy;
+			return *offset;
+		}
+	}
+	else
+		return 0;
 }
 
 static struct proc_ops proc_fops = {
-		.proc_read = proc_read,
+		.proc_read = procFS_read,
 };
 
 static int matrix_init(void) 
@@ -262,16 +449,18 @@ static int matrix_init(void)
 	}
 	pr_info("matrix: /proc/%s/%s created\n", PROC_DIR_NAME, PROC_FILE_NAME);
 	matrix_key_setup();
-	pr_info("|-----------------------matrix-----------------------------|\n");
-	pr_notice("|matrix-calculator: calculater is started                  |\n");
+	pr_calc(PR_GL_SEPARATOR);
+	pr_calc(PR_START_CALC);
 	matrix_check_key();
+	pr_calc(PR_GL_SEPARATOR);
+	output_choice_message();
+
 	return 0;
 // ERROR OPERATIONS
 proc_err:
 	proc_remove(proc_dir);
 device_err:
 	device_destroy(class_tree, MM);
-class_destroy:
 	class_destroy(class_tree);
 class_err:
 	cdev_del(&device);
