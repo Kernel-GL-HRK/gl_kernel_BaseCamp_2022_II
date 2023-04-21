eed8333a03f2600faed8c85bf8074ca5c0e55bf3
Vadym Minziuk <95727058+VadymMinziuk@users.noreply.github.com>
2023-04-21 14:38:37 +0300

    TASK02 (#70)
    Release bash scripts.
    Copying all source code from the project to /tmp/guesanumber
    Compress directory /tmp/guesanumber to gzip archive with the same name
    Copy gzip archive to “release” subdirectory in project dir
    Delete directory  /tmp/guesanumber
    Delete file guesanumber.tar.gz in working directory
    (Copy of guesanumber.tar.gz is in release subdirectory)
    This script which runs program "guess a number" in infinity loop
    Script counts users’ success attempts and prints their number.
    Each iteration script ask user to press "y" to continue, "n" to exit,
    or number to set number of runs of tries without asking "y or n"
    
    Signed-off-by: Vadym Minziuk <vadym.minziuk@gmail.com>
    
    ---------
    
    Signed-off-by: Oleksii Vodka <oleksii.vodka@gmail.com>
    Signed-off-by: Vadym Minziuk <vadym.minziuk@gmail.com>
    Co-authored-by: Oleksii Vodka <oleksii.vodka@gmail.com>

---
+++ archiver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+Color_Off='\033[0m' # Text Reset
+Red='\033[0;31m'    # Red
+On_White='\033[47m' # White
+
+echo -e "${Red}${On_White}To archive source code${Color_Off}"
+echo -e "${Red}${On_White}run this script from the project directory${Color_Off}"
+
+# Copy source code
+rm -rf /tmp/guesanumber 2>/dev/null
+mkdir /tmp/guesanumber
+cp --archive ./*.c ./*.h ./*.md ./*.sh /tmp/guesanumber 2>/dev/null
+
+#Compress the directory
+tar -zcvf guesanumber.tar.gz /tmp/guesanumber
+
+#Copy the archive to project subdirectory
+mkdir ./release 2>/dev/null
+cp -f ./guesanumber.tar.gz ./release
+
+#Clear temporary data
+rm -rf /tmp/guesanumber
+rm -f ./guesanumber.tar.gz
