fe7b2221e16fef5fda8226ba331662611dda4fae
Andrii Anoshyn <anoshyn.andrii@gmail.com>
2023-05-03 13:57:15 +0300

    Modify LED drive source with checkpatch
    Signed-off-by: Andrii Anoshyn <anoshyn.andrii@gmail.com>

--- task5-procfs-modules/led-gpio-proc.c
+++ task5-procfs-modules/led-gpio-proc.c
@@ -9,27 +9,28 @@
 #define PROC_FILENAME "led_blink"
 
 static struct timer_list blink_timer;
-static int led_status = 0;
+static int led_status;
 static int blink_period_ms = 500;
 
 static ssize_t led_proc_read(struct file *filp, char *buf, size_t count,
 		loff_t *offp)
 {
-    int len = 0;
-    len += sprintf(buf + len, "LED status: %d\n", led_status);
-    len += sprintf(buf + len, "Blink period: %d ms\n", blink_period_ms);
-    return len;
+	int len = 0;
+
+	len += sprintf(buf + len, "LED status: %d\n", led_status);
+	len += sprintf(buf + len, "Blink period: %d ms\n", blink_period_ms);
+	return len;
 }
 
 static ssize_t led_proc_write(struct file *file, const char *buf,
-                          size_t count, loff_t *offp)
+			size_t count, loff_t *offp)
 {
-    int period;
-    if (sscanf(buf, "%d", &period) != 1) {
-        return -EINVAL;
-    }
-    blink_period_ms = period;
-    return count;
+	int period;
+
+	if (kstrtoint(buf, 10, &period) != 1)
+		return -EINVAL;
+	blink_period_ms = period;
+	return count;
 }
 
 static const struct proc_ops led_proc_fops = {
@@ -39,50 +40,48 @@ static const struct proc_ops led_proc_fops = {
 
 static void blink_timer_func(struct timer_list *unused)
 {
-    gpio_set_value(LED, led_status);
-    led_status = !led_status;
-    blink_timer.expires = jiffies + msecs_to_jiffies(blink_period_ms);
-    add_timer(&blink_timer);
+	gpio_set_value(LED, led_status);
+	led_status = !led_status;
+	blink_timer.expires = jiffies + msecs_to_jiffies(blink_period_ms);
+	add_timer(&blink_timer);
 }
 
 static int __init led_init(void)
 {
-    int ret = 0;
+	int ret = 0;
 
-    ret = gpio_request(LED, "led1");
-    if (ret < 0) {
-        printk(KERN_ERR "Unable to request GPIO: %d\n", ret);
-        return ret;
-    }
+	ret = gpio_request(LED, "led1");
+	if (ret < 0) {
+		pr_err("Unable to request GPIO: %d\n", ret);
+		return ret;
+	}
+	ret = gpio_direction_output(LED, led_status);
+	if (ret < 0) {
+		pr_err("Unable to set GPIO direction: %d\n", ret);
+		gpio_free(LED);
+		return ret;
+	}
+	timer_setup(&blink_timer, blink_timer_func, 0);
+	blink_timer.expires = jiffies + msecs_to_jiffies(blink_period_ms);
+	add_timer(&blink_timer);
 
-    ret = gpio_direction_output(LED, led_status);
-    if (ret < 0) {
-        printk(KERN_ERR "Unable to set GPIO direction: %d\n", ret);
-        gpio_free(LED);
-        return ret;
-    }
+	if (!proc_create(PROC_FILENAME, 0, NULL, &led_proc_fops)) {
+		pr_err("Unable to create /proc/%s\n", PROC_FILENAME);
+		gpio_free(LED);
+		return -ENOMEM;
+	}
 
-    timer_setup(&blink_timer, blink_timer_func, 0);
-    blink_timer.expires = jiffies + msecs_to_jiffies(blink_period_ms);
-    add_timer(&blink_timer);
-
-    if (!proc_create(PROC_FILENAME, 0, NULL, &led_proc_fops)) {
-        printk(KERN_ERR "Unable to create /proc/%s\n", PROC_FILENAME);
-        gpio_free(LED);
-        return -ENOMEM;
-    }
-
-    printk(KERN_INFO "LED blink module loaded\n");
-    return 0;
+	pr_info("LED blink module loaded\n");
+	return 0;
 }
 
 static void __exit led_exit(void)
 {
-    del_timer_sync(&blink_timer);
-    gpio_set_value(LED, 0);
-    gpio_free(LED);
-    remove_proc_entry(PROC_FILENAME, NULL);
-    printk(KERN_INFO "LED blink module unloaded\n");
+	del_timer_sync(&blink_timer);
+	gpio_set_value(LED, 0);
+	gpio_free(LED);
+	remove_proc_entry(PROC_FILENAME, NULL);
+	pr_info("LED blink module unloaded\n");
 }
 
 module_init(led_init);
